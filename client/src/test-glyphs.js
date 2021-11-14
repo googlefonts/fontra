478244b5894bac524d5d664226577892d8ef4d28
justvanrossum <justvanrossum@gmail.com>
2021-11-14 20:26:55 +0100

    add test glyph from MutatorSans

---
+++ client/src/test-glyphs.js
@@ -0,0 +1,114 @@
+const testGlyphs = {
+
+  lightCondensed: path => {
+    // MutatorMathTest LightCondensed
+    path.moveTo(250, 300);
+    path.lineTo(409, 300);
+    path.lineTo(409, 216);
+    path.lineTo(418, 0);
+    path.lineTo(446, 0);
+    path.lineTo(446, 335);
+    path.lineTo(250, 335);
+    path.closePath();
+    path.moveTo(410, 284);
+    path.curveTo(397, 85, 340, 26, 257, 26);
+    path.lineTo(246, 26);
+    path.curveTo(152, 26, 90, 104, 90, 352);
+    path.curveTo(90, 599, 161, 674, 255, 674);
+    path.lineTo(266, 674);
+    path.curveTo(348, 674, 396, 618, 410, 442);
+    path.lineTo(449, 447);
+    path.curveTo(432, 638, 370, 710, 266, 710);
+    path.lineTo(255, 710);
+    path.curveTo(134, 710, 50, 615, 50, 352);
+    path.curveTo(50, 88, 125, -10, 246, -10);
+    path.lineTo(257, -10);
+    path.curveTo(362, -10, 420, 64, 426, 278);
+    path.closePath();
+  },
+
+  lightWide: path => {
+    // MutatorMathTest LightWide
+    path.moveTo(470, 300);
+    path.lineTo(1229, 300);
+    path.lineTo(1229, 216);
+    path.lineTo(1247, 0);
+    path.lineTo(1266, 0);
+    path.lineTo(1266, 330);
+    path.lineTo(470, 330);
+    path.closePath();
+    path.moveTo(1233, 277);
+    path.curveTo(1193, 102, 1006, 26, 730, 26);
+    path.lineTo(630, 26);
+    path.curveTo(318, 26, 120, 124, 120, 352);
+    path.curveTo(120, 579, 318, 674, 630, 674);
+    path.lineTo(730, 674);
+    path.curveTo(1004, 674, 1190, 601, 1232, 431);
+    path.lineTo(1272, 435);
+    path.curveTo(1228, 621, 1027, 710, 730, 710);
+    path.lineTo(630, 710);
+    path.curveTo(292, 710, 80, 595, 80, 352);
+    path.curveTo(80, 108, 292, -10, 630, -10);
+    path.lineTo(730, -10);
+    path.curveTo(1029, -10, 1220, 82, 1263, 273);
+    path.closePath();
+  },
+
+  boldCondensed: path => {
+    // MutatorMathTest BoldCondensed
+    path.moveTo(420, 305);
+    path.lineTo(587, 305);
+    path.lineTo(587, 216);
+    path.lineTo(687, 0);
+    path.lineTo(800, 0);
+    path.lineTo(800, 430);
+    path.lineTo(420, 430);
+    path.closePath();
+    path.moveTo(532, 378);
+    path.curveTo(530, 284, 500, 254, 426, 254);
+    path.lineTo(418, 254);
+    path.curveTo(362, 254, 342, 298, 342, 403);
+    path.curveTo(342, 504, 366, 546, 433, 546);
+    path.lineTo(441, 546);
+    path.curveTo(505.0, 546, 530.3387986715655, 522.7540597795514, 532, 448);
+    path.lineTo(792, 457);
+    path.curveTo(773.0, 696.0, 640.0, 810, 434, 810);
+    path.lineTo(409, 810);
+    path.curveTo(170, 810, 20, 679, 20, 403);
+    path.curveTo(20, 124, 170, -10, 409, -10);
+    path.lineTo(434, -10);
+    path.curveTo(640, -10, 774, 107, 792, 350);
+    path.closePath();
+  },
+
+  boldWide: path => {
+    // MutatorMathTest BoldWide
+    path.moveTo(640, 315);
+    path.lineTo(907, 315);
+    path.lineTo(907, 216);
+    path.lineTo(1107, 0);
+    path.lineTo(1350, 0);
+    path.lineTo(1350, 420);
+    path.lineTo(640, 420);
+    path.closePath();
+    path.moveTo(906, 372);
+    path.curveTo(890, 296, 829, 254, 738, 254);
+    path.lineTo(644, 254);
+    path.curveTo(538, 254, 472, 298, 472, 403);
+    path.curveTo(472, 504, 538, 546, 644, 546);
+    path.lineTo(738, 546);
+    path.curveTo(828, 546, 889, 516, 906, 445);
+    path.lineTo(1333, 491);
+    path.curveTo(1291, 707, 1090, 810, 790, 810);
+    path.lineTo(590, 810);
+    path.curveTo(252, 810, 40, 679, 40, 403);
+    path.curveTo(40, 124, 252, -10, 590, -10);
+    path.lineTo(610, -10);
+    path.curveTo(912, -10, 1114, 107, 1154, 329);
+    path.closePath();
+  },
+
+}
+
+
+export default testGlyphs;
