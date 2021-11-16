aef6bea48220351e15e28a181c44e710939db944
justvanrossum <justvanrossum@gmail.com>
2021-11-16 22:26:37 +0100

    beginnings of var-model, partial port of fontTools.varLib.models

---
+++ client/src/var-model.js
@@ -0,0 +1,259 @@
+// Partial port of fontTools.varLib.models.VariationModel
+
+import { VariationError } from "./errors.js";
+import { addItemwise, subItemwise, mulScalar } from "./var-funcs.js"
+
+
+class VariationModel {
+
+  constructor(locations, axisOrder = null) {
+    this.locations = locations;
+    this.axisOrder = axisOrder;
+    const locationsSet = Set(locations.map(item => locationToString(item)));
+    if (locationsSet.size != locations.length) {
+      throw new VariationError("locations must be unique");
+    }
+    if (! locationsSet.has("{}")) {
+      throw new VariationError("locations must contain {} default");
+    }
+    const compareFunc = self.getMasterLocationsSortCompareFunc(locations, this.axisOrder);
+    const sortedLocations = locations.slice();
+    sortedLocations.sort(compareFunc);
+    // this.mapping = ...;
+    // this.reverseMapping = ...;
+    this._computeMasterSupports();
+  }
+
+  getDeltas(masterValues) {
+    if (masterValues.length !== this.deltaWeights.length) {
+      throw new VariationError("masterValues must have the same length as this.deltaWeights")
+    }
+    const mapping = this.reverseMapping;
+    const out = [];
+    for (i = 0; i < masterValues.length; i++) {
+      let delta = masterValues[mapping[i]];
+      const weights = this.deltaWeights[i];
+      for (let [j, weight] of weights.entries()) {
+        if (weight === 1) {
+          delta = subItemwise(delta, out[i]);
+        } else {
+          delta = subItemwise(delta, mulScalar(out[i], weight));
+        }
+        out.push(delta);
+      }
+    }
+    return out;
+  }
+
+  getScalars(loc) {
+    return this.supports.map(support => supportScalar(loc, support));
+  }
+
+  interpolateFromDeltas(loc, deltas) {
+    const scalars = this.getScalars(loc);
+    return interpolateFromDeltasAndScalars(deltas, scalars);
+  }
+
+}
+
+
+function locationToString(loc) {
+  const keys = Object.keys(loc);
+  const result = {};
+  keys.sort()
+  for (key of keys) {
+    result[key] = loc[key];
+  }
+  return JSON.stringify(result);
+}
+
+
+function normalizeValue(v, triple) {
+  // Normalizes value based on a min/default/max triple.
+  // >>> normalizeValue(400, (100, 400, 900))
+  // 0.0
+  // >>> normalizeValue(100, (100, 400, 900))
+  // -1.0
+  // >>> normalizeValue(650, (100, 400, 900))
+  // 0.5
+  const [lower, dflt, upper] = triple;
+  if (!((lower <= dflt) && (dflt <= upper))) {
+    throw VariationError(
+      `Invalid axis values, must be minimum, default, maximum: ${lower}, ${dflt}, ${upper}`
+    );
+  }
+  v = Math.max(Math.min(v, upper), lower);
+  if (v === dflt) {
+    v = 0.0;
+  } else if (v < dflt) {
+    v = (v - dflt) / (dflt - lower);
+  } else {
+    v = (v - dflt) / (upper - dflt);
+  }
+  return v
+}
+
+
+function normalizeLocation(location, axes) {
+  // Normalizes location based on axis min/default/max values from axes.
+  // >>> axes = {"wght": (100, 400, 900)}
+  // >>> normalizeLocation({"wght": 400}, axes)
+  // {'wght': 0.0}
+  // >>> normalizeLocation({"wght": 100}, axes)
+  // {'wght': -1.0}
+  // >>> normalizeLocation({"wght": 900}, axes)
+  // {'wght': 1.0}
+  // >>> normalizeLocation({"wght": 650}, axes)
+  // {'wght': 0.5}
+  // >>> normalizeLocation({"wght": 1000}, axes)
+  // {'wght': 1.0}
+  // >>> normalizeLocation({"wght": 0}, axes)
+  // {'wght': -1.0}
+  // >>> axes = {"wght": (0, 0, 1000)}
+  // >>> normalizeLocation({"wght": 0}, axes)
+  // {'wght': 0.0}
+  // >>> normalizeLocation({"wght": -1}, axes)
+  // {'wght': 0.0}
+  // >>> normalizeLocation({"wght": 1000}, axes)
+  // {'wght': 1.0}
+  // >>> normalizeLocation({"wght": 500}, axes)
+  // {'wght': 0.5}
+  // >>> normalizeLocation({"wght": 1001}, axes)
+  // {'wght': 1.0}
+  // >>> axes = {"wght": (0, 1000, 1000)}
+  // >>> normalizeLocation({"wght": 0}, axes)
+  // {'wght': -1.0}
+  // >>> normalizeLocation({"wght": -1}, axes)
+  // {'wght': -1.0}
+  // >>> normalizeLocation({"wght": 500}, axes)
+  // {'wght': -0.5}
+  // >>> normalizeLocation({"wght": 1000}, axes)
+  // {'wght': 0.0}
+  // >>> normalizeLocation({"wght": 1001}, axes)
+  // {'wght': 0.0}
+  const out = {};
+  for ([tag, triple] of Object.entries(axes)) {
+    v = location[tag] || triple[1];
+    out[tag] = normalizeValue(v, triple);
+  }
+  return out;
+}
+
+
+function supportScalar(location, support, ot=true) {
+  // Returns the scalar multiplier at location, for a master
+  // with support.  If ot is True, then a peak value of zero
+  // for support of an axis means "axis does not participate".  That
+  // is how OpenType Variation Font technology works.
+  // >>> supportScalar({}, {})
+  // 1.0
+  // >>> supportScalar({'wght':.2}, {})
+  // 1.0
+  // >>> supportScalar({'wght':.2}, {'wght':(0,2,3)})
+  // 0.1
+  // >>> supportScalar({'wght':2.5}, {'wght':(0,2,4)})
+  // 0.75
+  // >>> supportScalar({'wght':2.5, 'wdth':0}, {'wght':(0,2,4), 'wdth':(-1,0,+1)})
+  // 0.75
+  // >>> supportScalar({'wght':2.5, 'wdth':.5}, {'wght':(0,2,4), 'wdth':(-1,0,+1)}, ot=False)
+  // 0.375
+  // >>> supportScalar({'wght':2.5, 'wdth':0}, {'wght':(0,2,4), 'wdth':(-1,0,+1)})
+  // 0.75
+  // >>> supportScalar({'wght':2.5, 'wdth':.5}, {'wght':(0,2,4), 'wdth':(-1,0,+1)})
+  // 0.75
+  let scalar = 1.0;
+  for (let [axis, [lower, peak, upper]] of Object.entries(support)) {
+    let v;
+    if (ot) {
+      // OpenType-specific case handling
+      if (peak === 0.0) {
+        continue;
+      }
+      if (lower > peak || peak > upper) {
+        continue;
+      }
+      if (lower < 0.0 && upper > 0.0) {
+        continue;
+      }
+      v = location[axis] || 0.0;
+    } else {
+      if (location[axis] === undefined) {
+        throw new VariationError(`axes ${axis} not present in location`);
+      }
+      v = location[axis];
+    }
+    if (v === peak) {
+      continue;
+    }
+    if (v <= lower || upper <= v) {
+      scalar = 0.0;
+      break;
+    }
+    if (v < peak) {
+      scalar *= (v - lower) / (peak - lower);
+    } else {
+      // v > peak
+      scalar *= (v - upper) / (peak - upper);
+    }
+  }
+  return scalar
+}
+
+
+function interpolateFromDeltasAndScalars(deltas, scalars) {
+  if (deltas.length !== scalars.length) {
+    throw new VariationError("deltas and scalars must have the same length");
+  }
+  let v = null;
+  for (i = 0; i < scalars.length; i++) {
+    const scalar = scalars[i];
+    if (!scalar) {
+      continue;
+    }
+    const contribution = mulScalar(deltas[i], scalar);
+    if (v === null) {
+      v = contribution;
+    } else {
+      v = addItemwise(a, contribution);
+    }
+  }
+  return v;
+}
+
+
+function deepCompare(a, b) {
+  if (typeof a === "string" || typeof a === "number") {
+    if (typeof a !== typeof b) {
+      throw new TypeError("can't compare objects");
+    }
+    if (a < b) {
+      return -1;
+    } else if (a === b) {
+      return 0;
+    } else {
+      // a > b
+      return 1;
+    }
+  } else {
+    if (a.length === undefined || b.length === undefined) {
+      throw new TypeError("can't compare objects");
+    }
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+      const itemA = a[i];
+      const itemB = b[i];
+      if (itemA === undefined) {
+        return -1;
+      } else if (itemB === undefined) {
+        return 1;
+      } else {
+        const result = deepCompare(itemA, itemB);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return 0;
+  }
+}
+
+export { VariationModel, deepCompare, locationToString, normalizeLocation };
---
+++ client/test/var-model.js
@@ -0,0 +1,65 @@
+import chai from "chai";
+const expect = chai.expect;
+
+import { deepCompare } from "../src/var-model.js";
+
+
+describe("var-model tests", () => {
+
+  describe("deepCompare tests", () => {
+    it("deepCompare scalars", () => {
+      expect(deepCompare(1, 2)).to.equal(-1);
+      expect(deepCompare(9, 10)).to.equal(-1);
+      expect(deepCompare(3, 2)).to.equal(1);
+      expect(deepCompare(3, 3)).to.equal(0);
+      expect(deepCompare("a", "b")).to.equal(-1);
+      expect(deepCompare("a", "aa")).to.equal(-1);
+      expect(deepCompare("aa", "a")).to.equal(1);
+      expect(deepCompare("aaa", "aaa")).to.equal(0);
+      expect(deepCompare("9", "10")).to.equal(1);
+    });
+
+    it("deepCompare empty array", () => {
+      expect(deepCompare([], [])).to.equal(0);
+    });
+
+    it("deepCompare string array", () => {
+      expect(deepCompare(["a", "aa"], ["a", "aa"])).to.equal(0);
+      expect(deepCompare(["a", "aa"], ["a", "aaa"])).to.equal(-1);
+      expect(deepCompare(["a", "aaaa"], ["a", "aaa"])).to.equal(1);
+    });
+
+    it("deepCompare numeric array", () => {
+      expect(deepCompare([1, 2], [1, 2])).to.equal(0);
+      expect(deepCompare([1, 2], [2, 2])).to.equal(-1);
+      expect(deepCompare([1, 2], [1, 3])).to.equal(-1);
+      expect(deepCompare([1, 4], [1, 3])).to.equal(1);
+      expect(deepCompare([2, 2], [1, 3])).to.equal(1);
+    });
+
+    it("deepCompare numeric array", () => {
+      expect(deepCompare([1, 2], [1, 2])).to.equal(0);
+      expect(deepCompare([1, 2], [2, 2])).to.equal(-1);
+      expect(deepCompare([1, 2], [1, 2, 3])).to.equal(-1);
+      expect(deepCompare([1, 2], [1, 3])).to.equal(-1);
+      expect(deepCompare([1, 4], [1, 3])).to.equal(1);
+      expect(deepCompare([2, 2], [1, 3])).to.equal(1);
+      const l = [[1, 2, 3], [1, 2]];
+      l.sort(deepCompare);
+      expect(l).to.deep.equal([[1, 2], [1, 2, 3]]);
+    });
+
+    it("deepCompare nested numeric array", () => {
+      expect(deepCompare([1, [3, 4]], [1, [3, 4]])).to.equal(0);
+      expect(deepCompare([1, [3, 4]], [1, [3, 5]])).to.equal(-1);
+      expect(deepCompare([1, [3, 4]], [1, [3, 3]])).to.equal(1);
+    });
+
+    it("deepCompare throw TypeError", () => {
+      expect(() => deepCompare({}, [])).to.throw(TypeError);
+      expect(() => deepCompare(123, "123")).to.throw(TypeError);
+    });
+
+  });
+
+});
