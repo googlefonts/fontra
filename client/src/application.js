16231e8a31a6ec3966b312f0ad91ba2ee28ca86b
justvanrossum <justvanrossum@gmail.com>
2021-11-20 08:47:00 +0100

    use arrow func

--- client/src/application.js
+++ client/src/application.js
@@ -3,7 +3,7 @@ import RemoteObject from "./remote.js";
 
 export default function getApplication(wsURL) {
   const app = new Proxy(new RemoteObject(wsURL), {
-    get: function(remote, propertyName, app) {
+    get: (remote, propertyName, app) => {
       return function () {
         return remote.doCall(propertyName, Array.from(arguments))
       };
