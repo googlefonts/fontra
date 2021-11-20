0048048479c112f1bd7adab0ff8402371f1bed44
justvanrossum <justvanrossum@gmail.com>
2021-11-20 08:45:36 +0100

    make app a proxy for a remote object; refactore remote object

--- client/src/application.js
+++ client/src/application.js
@@ -1,60 +1,13 @@
-export default class Application {
+import RemoteObject from "./remote.js";
 
-  constructor(wsUrl) {
-      this.websocket = new WebSocket(wsUrl);
-      this.websocket.onmessage = (event) => this._handleIncomingMessage(event);
-      this._callReturnCallbacks = {};
 
-      const g =_genNextCallID();
-      this._getNextCallID = () => {return g.next().value};
-  }
-
-  getGlyph(glyphName) {
-    return this._doCall("getGlyph", [glyphName])
-  }
-
-  _handleIncomingMessage(event) {
-    const message = JSON.parse(event.data);
-    const callID = message["call-id"];
-
-    console.log("incoming message");
-    console.log(message);
-    if (callID !== undefined) {
-      const returnCallbacks = this._callReturnCallbacks[callID];
-      if (message.exception !== undefined) {
-        returnCallbacks.reject(message["exception"]);
-      } else {
-        returnCallbacks.resolve(message["return-value"]);
-      }
-      delete this._callReturnCallbacks[callID];
+export default function getApplication(wsURL) {
+  const app = new Proxy(new RemoteObject(wsURL), {
+    get: function(remote, propertyName, app) {
+      return function () {
+        return remote.doCall(propertyName, Array.from(arguments))
+      };
     }
-  }
-
-  _doCall(methodName, args) {
-    const callID = this._getNextCallID();
-    const message = {
-      "call-id": callID,
-      "method-name": methodName,
-      "arguments": args,
-    };
-    console.log("outgoing")
-    console.log(message)
-    this.websocket.send(JSON.stringify(message));
-
-    this._callReturnCallbacks[callID] = {}
-    return new Promise((resolve, reject) => {
-      this._callReturnCallbacks[callID]["resolve"] = resolve;
-      this._callReturnCallbacks[callID]["reject"] = reject;
-    });
-  }
-
-}
-
-
-function* _genNextCallID() {
-  let callID = 0;
-  while (true) {
-    yield callID;
-    callID++;
-  }
+  });
+  return app;
 }
---
+++ client/src/remote.js
@@ -0,0 +1,58 @@
+export default class RemoteObject {
+
+  constructor(wsURL) {
+      this.websocket = new WebSocket(wsURL);
+      this.websocket.onmessage = (event) => this._handleIncomingMessage(event);
+      this._callReturnCallbacks = {};
+
+      const g =_genNextCallID();
+      this._getNextCallID = () => {return g.next().value};
+  }
+
+  _handleIncomingMessage(event) {
+    const message = JSON.parse(event.data);
+    const callID = message["call-id"];
+
+    console.log("incoming message");
+    console.log(message);
+    if (callID !== undefined) {
+      const returnCallbacks = this._callReturnCallbacks[callID];
+      if (message.exception !== undefined) {
+        returnCallbacks.reject(message["exception"]);
+      } else {
+        returnCallbacks.resolve(message["return-value"]);
+      }
+      delete this._callReturnCallbacks[callID];
+    }
+  }
+
+  doCall(methodName, args) {
+    const callID = this._getNextCallID();
+    const message = {
+      "call-id": callID,
+      "method-name": methodName,
+      "arguments": args,
+    };
+    console.log("outgoing")
+    console.log(message)
+    console.log("args", args);
+    console.log("args", JSON.stringify(args));
+    this.websocket.send(JSON.stringify(message));
+
+    this._callReturnCallbacks[callID] = {}
+    return new Promise((resolve, reject) => {
+      this._callReturnCallbacks[callID]["resolve"] = resolve;
+      this._callReturnCallbacks[callID]["reject"] = reject;
+    });
+  }
+
+}
+
+
+function* _genNextCallID() {
+  let callID = 0;
+  while (true) {
+    yield callID;
+    callID++;
+  }
+}
