7fd58977a94544194ca699b5b4119d4b6a473bbd
justvanrossum <justvanrossum@gmail.com>
2021-11-22 16:37:17 +0100

    add set operations

---
+++ client/src/set-ops.js
@@ -0,0 +1,50 @@
+// Copied straight from
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+
+export function isSuperset(set, subset) {
+  for (let elem of subset) {
+    if (!set.has(elem)) {
+      return false
+    }
+  }
+  return true
+}
+
+export function union(setA, setB) {
+  let _union = new Set(setA)
+  for (let elem of setB) {
+    _union.add(elem)
+  }
+  return _union
+}
+
+export function intersection(setA, setB) {
+  let _intersection = new Set()
+  for (let elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem)
+    }
+  }
+  return _intersection
+}
+
+export function symmetricDifference(setA, setB) {
+  let _difference = new Set(setA)
+  for (let elem of setB) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem)
+    } else {
+      _difference.add(elem)
+    }
+  }
+  return _difference
+}
+
+export function difference(setA, setB) {
+  let _difference = new Set(setA)
+  for (let elem of setB) {
+    _difference.delete(elem)
+  }
+  return _difference
+}
