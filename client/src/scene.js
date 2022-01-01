29afc5239f7771a1e77b668f07c61009e0632254
justvanrossum <justvanrossum@gmail.com>
2022-01-01 17:51:16 +0100

    initial new scene stuff

---
+++ client/src/scene.js
@@ -0,0 +1,28 @@
+export class SceneModel {
+  constructor() {
+
+  }
+}
+
+
+export class SceneView {
+
+  constructor(model, drawFunc) {
+    this.model = model;
+    this.drawFunc = drawFunc;
+    this.subviews = [];
+    this.visible = true;
+  }
+
+  draw(canvasController) {
+    if (!this.visible) {
+      return;
+    }
+    this.subviews.forEach(view => view.draw(canvasController));
+    if (this.drawFunc === undefined) {
+      return;
+    }
+    this.drawFunc(this.model, canvasController);
+  }
+
+}
