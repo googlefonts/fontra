92c0a6114d1deb5c0e83f2328064a1a28fe24622
justvanrossum <justvanrossum@gmail.com>
2022-02-27 16:10:25 +0100

    maintain glyph dependencies

--- server/src/fontra/fonthandler.py
+++ server/src/fontra/fonthandler.py
@@ -17,13 +17,20 @@ class FontHandler:
             "getGlobalAxes",
             "subscribeGlyphChanges",
         }
+        self.glyphUsedBy = {}
+        self.glyphMadeOf = {}
 
     def getGlyph(self, glyphName, *, client):
         return self._getGlyph(glyphName)
 
     @functools.lru_cache(250)
     def _getGlyph(self, glyphName):
-        return asyncio.create_task(self.backend.getGlyph(glyphName))
+        return asyncio.create_task(self._getGlyphFromBackend(glyphName))
+
+    async def _getGlyphFromBackend(self, glyphName):
+        glyphData = await self.backend.getGlyph(glyphName)
+        self.updateGlyphDependencies(glyphName, glyphData)
+        return glyphData
 
     async def getGlyphNames(self, *, client):
         return await self.backend.getGlyphNames()
@@ -60,3 +67,25 @@ class FontHandler:
         await asyncio.gather(
             *[client.proxy.externalChange(change) for client in clients]
         )
+
+    def updateGlyphDependencies(self, glyphName, glyphData):
+        # Zap previous used-by data for this glyph, if any
+        for componentName in self.glyphMadeOf.get(glyphName, ()):
+            if componentName in self.glyphUsedBy:
+                self.glyphUsedBy[componentName].discard(glyphName)
+        componentNames = set(_iterAllComponentNames(glyphData))
+        if componentNames:
+            self.glyphMadeOf[glyphName] = componentNames
+        elif glyphName in self.glyphMadeOf:
+            del self.glyphMadeOf[glyphName]
+        for componentName in componentNames:
+            if componentName not in self.glyphUsedBy:
+                self.glyphUsedBy[componentName] = set()
+            self.glyphUsedBy[componentName].add(glyphName)
+
+
+def _iterAllComponentNames(glyphData):
+    for source in glyphData["sources"]:
+        for layer in source["layers"]:
+            for compo in layer["glyph"].get("components", ()):
+                yield compo["name"]
