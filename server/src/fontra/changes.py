97dcea2ee7550bfc1728d72df91cfa4c35cdf1fa
justvanrossum <justvanrossum@gmail.com>
2022-03-12 19:52:04 +0100

    move changes stuff to changes.py

---
+++ server/src/fontra/changes.py
@@ -0,0 +1,48 @@
+def setItem(subject, key, value):
+    subject[key] = value
+
+
+baseChangeFunctions = {
+    "=": setItem,
+}
+
+
+#
+# A "change" object is a simple JS object containing several
+# keys.
+#
+# "p": an array of path items, eg. ["glyphs", "Aring"]
+# Optional: can be omitted if empty.
+#
+# "f": function name, to be lookud up in the changeFunctions dict
+# Optional: can be omitted if the change has children
+#
+# "k": a key or index into the "subject"
+#
+# "v": "value", a single argument for the change function
+# "a": "arguments", an array of arguments for the change function
+# If the change has a change function ("f" key), it MUST also have
+# a "v" key/value or an "a" key/value, but NOT both
+#
+# "c": Array of child changes. Optional.
+#
+
+
+def applyChange(subject, change, changeFunctions):
+    path = change.get("p", [])
+    functionName = change.get("f")
+    children = change.get("c", [])
+
+    for pathElement in path:
+        subject = subject[pathElement]
+
+    if functionName is not None:
+        changeFunc = changeFunctions[functionName]
+        arg = change.get("v")
+        if arg is not None:
+            changeFunc(subject, change["k"], arg)
+        else:
+            changeFunc(subject, change["k"], *change["a"])
+
+    for subChange in children:
+        applyChange(subject, subChange, changeFunctions)
--- server/src/fontra/fonthandler.py
+++ server/src/fontra/fonthandler.py
@@ -1,6 +1,7 @@
 import asyncio
 from collections import defaultdict
 import functools
+from .changes import applyChange, baseChangeFunctions
 
 
 class FontHandler:
@@ -147,52 +148,9 @@ def setPointPosition(path, pointIndex, x, y):
     coords[i + 1] = y
 
 
-def setItem(subject, key, value):
-    subject[key] = value
-
-
 glyphChangeFunctions = {
     "=xy": setPointPosition,
-    "=": setItem,
 }
 
 
-#
-# A "change" object is a simple JS object containing several
-# keys.
-#
-# "p": an array of path items, eg. ["glyphs", "Aring"]
-# Optional: can be omitted if empty.
-#
-# "f": function name, to be lookud up in the changeFunctions dict
-# Optional: can be omitted if the change has children
-#
-# "k": a key or index into the "subject"
-#
-# "v": "value", a single argument for the change function
-# "a": "arguments", an array of arguments for the change function
-# If the change has a change function ("f" key), it MUST also have
-# a "v" key/value or an "a" key/value, but NOT both
-#
-# "c": Array of child changes. Optional.
-#
-
-
-def applyChange(subject, change, changeFunctions):
-    path = change.get("p", [])
-    functionName = change.get("f")
-    children = change.get("c", [])
-
-    for pathElement in path:
-        subject = subject[pathElement]
-
-    if functionName is not None:
-        changeFunc = changeFunctions[functionName]
-        arg = change.get("v")
-        if arg is not None:
-            changeFunc(subject, change["k"], arg)
-        else:
-            changeFunc(subject, change["k"], *change["a"])
-
-    for subChange in children:
-        applyChange(subject, subChange, changeFunctions)
+glyphChangeFunctions.update(baseChangeFunctions)
