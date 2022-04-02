272445c60902cb26e444139ea0d15bcd377b69bf
justvanrossum <justvanrossum@gmail.com>
2022-04-02 19:57:26 +0200

    put logger name back into the log format

--- server/src/fontra/__main__.py
+++ server/src/fontra/__main__.py
@@ -16,7 +16,7 @@ def existingFolder(path):
 
 def main():
     logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s",
+        format="%(asctime)s %(name)-17s %(levelname)-8s %(message)s",
         level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
