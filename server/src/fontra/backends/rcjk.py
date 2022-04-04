ad8f8bbbd49b6db89b88df426ee48145b32009e8
justvanrossum <justvanrossum@gmail.com>
2022-04-04 10:23:49 +0200

    refactor/rework to be independent of rcjktools, but share more with rcjkmysql.py

--- server/src/fontra/backends/rcjk.py
+++ server/src/fontra/backends/rcjk.py
@@ -1,25 +1,48 @@
-from rcjktools.project import RoboCJKProject
+import json
+import pathlib
 from .pen import PathBuilderPointPen
+from .ufo_utils import extractGlyphNameAndUnicodes
+from .rcjkmysql import GLIFGlyph, getComponentAxisDefaults, serializeGlyph
+
+
+glyphSetNames = ["characterGlyph", "deepComponent", "atomicElement"]
 
 
 class RCJKBackend:
     @classmethod
     def fromPath(cls, path):
-        self = cls()
-        self.project = RoboCJKProject(path)
+        return cls(path)
+
+    def __init__(self, path):
+        self.path = pathlib.Path(path).resolve()
+        for name in glyphSetNames:
+            setattr(self, name + "GlyphSet", RCJKGlyphSet(self.path / name))
+
+        if not self.characterGlyphGlyphSet.exists():
+            raise TypeError(f"Not a valid rcjk project: '{path}'")
+
+        designspacePath = self.path / "designspace.json"
+        if designspacePath.is_file():
+            self.designspace = json.loads(designspacePath.read_bytes())
+        else:
+            self.designspace = {}
+
         self.reversedCmap = {}
         for gs, hasEncoding in self._iterGlyphSets():
-            reversedCmap = gs.getGlyphNamesAndUnicodes()
+            reversedCmap = gs.getGlyphNamesAndUnicodes(not hasEncoding)
             for glyphName, unicodes in reversedCmap.items():
                 assert glyphName not in self.reversedCmap
                 self.reversedCmap[glyphName] = unicodes if hasEncoding else []
         self.glyphNames = sorted(self.reversedCmap)
-        return self
+
+        self._tempGlyphCache = {}
+        self._tempGlyphCacheTimer = None
+        self._tempGlyphCacheTimeout = 5
 
     def _iterGlyphSets(self):
-        yield self.project.characterGlyphGlyphSet, True
-        yield self.project.deepComponentGlyphSet, False
-        yield self.project.atomicElementGlyphSet, False
+        yield self.characterGlyphGlyphSet, True
+        yield self.deepComponentGlyphSet, False
+        yield self.atomicElementGlyphSet, False
 
     async def getReverseCmap(self):
         return self.reversedCmap
@@ -28,7 +51,7 @@ class RCJKBackend:
         axes = getattr(self, "_globalAxes", None)
         if axes is None:
             axes = []
-            for axis in self.project.designspace["axes"]:
+            for axis in self.designspace.get("axes", ()):
                 axis = dict(axis)
                 axis["label"] = axis["name"]
                 axis["name"] = axis["tag"]
@@ -38,94 +61,88 @@ class RCJKBackend:
         return axes
 
     async def getGlyph(self, glyphName):
-        glyph = self._getRCJKGlyph(glyphName)
-        if glyph is not None:
-            ensureComponentCoords(glyph, self._getRCJKGlyph)
-            glyph = serializeGlyph(glyph)
-        return glyph
+        layerGlyphs = self._getLayerGlyphs(glyphName)
+        axisDefaults = getComponentAxisDefaults(layerGlyphs, self._tempGlyphCache)
+        return serializeGlyph(layerGlyphs, axisDefaults)
 
-    def _getRCJKGlyph(self, glyphName):
-        if not glyphName:
-            return None
+    def _getLayerGlyphs(self, glyphName):
+        layerGlyphs = self._tempGlyphCache.get(glyphName)
+        if layerGlyphs is None:
+            self._populateGlyphCache(glyphName)
+            # self._scheduleCachePurge()
+            layerGlyphs = self._tempGlyphCache[glyphName]
+        return layerGlyphs
+
+    def _populateGlyphCache(self, glyphName):
+        if glyphName in self._tempGlyphCache:
+            return
+        layerGLIFData = self._getLayerGLIFData(glyphName)
+        if layerGLIFData is None:
+            return
+
+        layerGlyphs = {}
+        for layerName, glifData in layerGLIFData:
+            layerGlyphs[layerName] = GLIFGlyph.fromGLIFData(glifData)
+        self._tempGlyphCache[glyphName] = layerGlyphs
+
+        for compoName in layerGlyphs["foreground"].getComponentNames():
+            self._populateGlyphCache(compoName)
+
+    def _getLayerGLIFData(self, glyphName):
         for gs, _ in self._iterGlyphSets():
             if glyphName in gs:
-                return gs.getGlyph(glyphName)
-        else:
+                return gs.getGlyphLayerData(glyphName)
+        return None
+
+
+class RCJKGlyphSet:
+    def __init__(self, path):
+        self.path = path
+        self.revCmap = None
+        self.contents = {}
+        self.layers = {}
+        self.setupLayers()
+
+    def exists(self):
+        return self.path.is_dir()
+
+    def setupLayers(self):
+        if not self.exists():
+            return
+        for layerDir in self.path.iterdir():
+            if layerDir.is_dir():
+                glifPaths = {
+                    glifPath.name: glifPath for glifPath in layerDir.glob("*.glif")
+                }
+                if glifPaths:
+                    self.layers[layerDir.name] = glifPaths
+
+    def getGlyphNamesAndUnicodes(self, ignoreUnicodes=False):
+        if self.revCmap is None:
+            glyphNames = {}
+            for path in self.path.glob("*.glif"):
+                with open(path, "rb") as f:
+                    # assuming all unicodes are in the first 1024 bytes of the file
+                    data = f.read(1024)
+                glyphName, unicodes = extractGlyphNameAndUnicodes(data, path.name)
+                if ignoreUnicodes:
+                    unicodes = []
+                glyphNames[glyphName] = unicodes
+                self.contents[glyphName] = path
+            self.revCmap = glyphNames
+        return self.revCmap
+
+    def __contains__(self, glyphName):
+        return glyphName in self.contents
+
+    def getGlyphLayerData(self, glyphName):
+        mainPath = self.contents.get(glyphName)
+        if mainPath is None:
             return None
-
-
-def ensureComponentCoords(glyph, getGlyphFunc):
-    if getattr(glyph, "_ensuredComponentCoords", False):
-        return
-
-    for compoIndex, compo in enumerate(glyph.components):
-        compoGlyph = getGlyphFunc(compo.name)
-        if compoGlyph is None:
-            print(f"can't find component base glyph {compo.name}")
-            continue
-
-        allAxisNames = {
-            axisName
-            for g in [glyph] + glyph.variations
-            for axisName in g.components[compoIndex].coord
-        }
-        allAxisNames |= set(compoGlyph.axes)
-        for axisName in sorted(allAxisNames):
-            defaultFallbackValue = compoGlyph.axes.get(axisName, (0, 0, 0))[1]
-            defaultValue = glyph.components[compoIndex].coord.get(
-                axisName, defaultFallbackValue
-            )
-            for g in [glyph] + glyph.variations:
-                axisValue = g.components[compoIndex].coord.get(axisName)
-                if axisValue is None:
-                    g.components[compoIndex].coord[axisName] = defaultValue
-
-    glyph._ensuredComponentCoords = True
-
-
-def serializeGlyph(glyph):
-    d = {}
-    d["axes"] = [
-        dict(name=name, minValue=minValue, defaultValue=defaultValue, maxValue=maxValue)
-        for name, (minValue, defaultValue, maxValue) in glyph.axes.items()
-    ]
-    d["name"] = glyph.name
-    d["unicodes"] = glyph.unicodes
-    sources = []
-    layers = []
-    for varGlyph in [glyph] + glyph.variations:
-        source = {}
-        sourceName = getattr(varGlyph, "sourceName", "<default>")
-        if not sourceName and varGlyph.layerName:
-            sourceName = f"{varGlyph.layerName}"
-        source["name"] = sourceName
-        source["location"] = varGlyph.location
-        sourceGlyph = {}
-        path = serializePath(varGlyph)
-        if path is not None:
-            sourceGlyph["path"] = path
-        components = serializeComponents(varGlyph.components)
-        if components:
-            sourceGlyph["components"] = components
-        # TODO anchors?
-        sourceGlyph["xAdvance"] = varGlyph.width  # TODO: yAdvance, verticalOrigin
-        source["layerName"] = f"{sourceName}/foreground"
-        layers.append({"name": f"{sourceName}/foreground", "glyph": sourceGlyph})
-        sources.append(source)
-    d["sources"] = sources
-    d["layers"] = layers
-    return d
-
-
-def serializePath(glyph):
-    pen = PathBuilderPointPen()
-    glyph.drawPoints(pen)
-    assert not pen.components
-    return pen.getPath()
-
-
-def serializeComponents(components):
-    return [
-        dict(name=compo.name, transformation=compo.transform, location=compo.coord)
-        for compo in components
-    ]
+        mainFileName = mainPath.name
+        glyphLayerData = [("foreground", mainPath.read_bytes())]
+        for layerName, layerContents in self.layers.items():
+            layerPath = layerContents.get(mainFileName)
+            if layerPath is not None:
+                glyphLayerData.append((layerName, layerPath.read_bytes()))
+        return glyphLayerData
