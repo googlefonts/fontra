bec30c27e4b9f6931d473919ca394052567b71a7
Just van Rossum <justvanrossum@gmail.com>
2022-04-17 16:25:49 +0200

    start using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,50 +1,5 @@
-from setuptools import setup, find_packages
+# This is just a stub; see pyproject.toml for the metadata.
 
+from setuptools import setup
 
-setup(
-    name="fontra",
-    use_scm_version={"write_to": "src/fontra/_version.py"},
-    description="Fontra",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    author="Just van Rossum",
-    author_email="justvanrossum@gmail.com",
-    entry_points={
-        "console_scripts": ["fontra=fontra.__main__:main"],
-    },
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    package_data={
-        "": [
-            "client/*.html",
-            "client/*.ico",
-            "client/*/*.css",
-            "client/*/*.html",
-            "client/*/*.js",
-            "client/*/*.svg",
-            "client/*/*.woff2",
-        ]
-    },
-    install_requires=[
-        "aiohttp>=3.8.1",
-        "fonttools[ufo,unicode]>=4.17.0",
-        "watchfiles>=0.10",
-        "websockets>=10.1",
-    ],
-    setup_requires=["setuptools_scm"],
-    extras_require={},
-    python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Console",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Graphics",
-    ],
-)
+setup()
