b778a5b7766b7e19e3840eedb8a356cc8f886689
Just van Rossum <justvanrossum@gmail.com>
2022-05-13 11:40:29 +0200

    ensure we convert to float when getting a number value

--- src/fontra/client/core/ui-form.js
+++ src/fontra/client/core/ui-form.js
@@ -89,7 +89,7 @@ export class Form {
     inputElement.disabled = fieldItem.disabled;
     inputElement.onchange = event => {
       this._dispatchEvent("beginChange", {"key": fieldItem.key});
-      this._dispatchEvent("doChange", {"key": fieldItem.key, "value": inputElement.value});
+      this._dispatchEvent("doChange", {"key": fieldItem.key, "value": parseFloat(inputElement.value)});
       this._dispatchEvent("endChange", {"key": fieldItem.key});
     };
     this._fieldGetters[fieldItem.key] = () => inputElement.value;
@@ -118,7 +118,7 @@ export class Form {
         },
         change: () => {
           inputElement.value = myRound(sliderElement.value, 3);
-          this._dispatchEvent("doChange", {"key": fieldItem.key, "value": inputElement.value});
+          this._dispatchEvent("doChange", {"key": fieldItem.key, "value": parseFloat(inputElement.value)});
         },
         endEdit: () => {
           this._dispatchEvent("endChange", {"key": fieldItem.key});
