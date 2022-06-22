9a7f19dc4ca068b93f3aa3b9d060fdd780058ec0
Just van Rossum <justvanrossum@gmail.com>
2022-06-22 11:31:25 +0200

    fontra should not test fontra-rcjk

--- test-py/test_font.py
+++ test-py/test_font.py
@@ -337,306 +337,10 @@ getGlyphTestData = [
             ],
         },
     ),
-    (
-        "rcjk",
-        {
-            "axes": [
-                {"defaultValue": 0.0, "maxValue": 1.0, "minValue": 0.0, "name": "HLON"},
-                {"defaultValue": 0.0, "maxValue": 1.0, "minValue": 0.0, "name": "WGHT"},
-            ],
-            "name": "one_00",
-            "unicodes": [],
-            "sources": [
-                {
-                    "name": "<default>",
-                    "location": {},
-                    "layerName": "foreground",
-                },
-                {
-                    "name": "longbar",
-                    "location": {"HLON": 1.0},
-                    "layerName": "longbar",
-                },
-                {
-                    "name": "bold",
-                    "location": {"WGHT": 1.0},
-                    "layerName": "bold",
-                },
-            ],
-            "layers": [
-                {
-                    "name": "foreground",
-                    "glyph": {
-                        "path": {
-                            "coordinates": [
-                                105,
-                                0,
-                                134,
-                                0,
-                                134,
-                                600,
-                                110,
-                                600,
-                                92,
-                                600,
-                                74,
-                                598,
-                                59,
-                                596,
-                                30,
-                                592,
-                                30,
-                                572,
-                                105,
-                                572,
-                            ],
-                            "pointTypes": [0, 0, 0, 8, 2, 2, 8, 0, 0, 0],
-                            "contourInfo": [{"endPoint": 9, "isClosed": True}],
-                        },
-                        "xAdvance": 229,
-                    },
-                },
-                {
-                    "name": "bold",
-                    "glyph": {
-                        "path": {
-                            "coordinates": [
-                                135,
-                                0,
-                                325,
-                                0,
-                                325,
-                                600,
-                                170,
-                                600,
-                                152,
-                                600,
-                                135,
-                                598,
-                                119,
-                                596,
-                                20,
-                                582,
-                                20,
-                                457,
-                                135,
-                                457,
-                            ],
-                            "pointTypes": [0, 0, 0, 8, 2, 2, 8, 0, 0, 0],
-                            "contourInfo": [{"endPoint": 9, "isClosed": True}],
-                        },
-                        "xAdvance": 450,
-                    },
-                },
-                {
-                    "name": "longbar",
-                    "glyph": {
-                        "path": {
-                            "coordinates": [
-                                175,
-                                0,
-                                204,
-                                0,
-                                204,
-                                600,
-                                180,
-                                600,
-                                152,
-                                600,
-                                124,
-                                598,
-                                99,
-                                597,
-                                0,
-                                592,
-                                0,
-                                572,
-                                175,
-                                572,
-                            ],
-                            "pointTypes": [0, 0, 0, 8, 2, 2, 8, 0, 0, 0],
-                            "contourInfo": [{"endPoint": 9, "isClosed": True}],
-                        },
-                        "xAdvance": 369,
-                    },
-                },
-            ],
-        },
-    ),
-    (
-        "rcjk",
-        {
-            "axes": [
-                {"defaultValue": 0.0, "maxValue": 1.0, "minValue": 0.0, "name": "wght"}
-            ],
-            "name": "uni0031",
-            "unicodes": [49],
-            "sources": [
-                {
-                    "name": "<default>",
-                    "location": {},
-                    "layerName": "foreground",
-                },
-                {
-                    "name": "wght",
-                    "location": {"wght": 1.0},
-                    "layerName": "wght",
-                },
-            ],
-            "layers": [
-                {
-                    "name": "foreground",
-                    "glyph": {
-                        "components": [
-                            {
-                                "name": "DC_0031_00",
-                                "transformation": {
-                                    "rotation": 0,
-                                    "scalex": 1,
-                                    "scaley": 1,
-                                    "tcenterx": 0,
-                                    "tcentery": 0,
-                                    "x": -1,
-                                    "y": 0,
-                                },
-                                "location": {"T_H_lo": 0, "X_X_bo": 0},
-                            }
-                        ],
-                        "xAdvance": 350,
-                    },
-                },
-                {
-                    "name": "wght",
-                    "glyph": {
-                        "components": [
-                            {
-                                "name": "DC_0031_00",
-                                "transformation": {
-                                    "rotation": 0,
-                                    "scalex": 0.93,
-                                    "scaley": 1,
-                                    "tcenterx": 0,
-                                    "tcentery": 0,
-                                    "x": -23.0,
-                                    "y": 0.0,
-                                },
-                                "location": {"T_H_lo": 0, "X_X_bo": 0.7},
-                            }
-                        ],
-                        "xAdvance": 350,
-                    },
-                },
-            ],
-        },
-    ),
-    (
-        "rcjk",
-        {
-            "axes": [
-                {
-                    "defaultValue": 0.0,
-                    "maxValue": 1.0,
-                    "minValue": 0.0,
-                    "name": "X_X_bo",
-                },
-                {
-                    "defaultValue": 0.0,
-                    "maxValue": 1.0,
-                    "minValue": 0.0,
-                    "name": "X_X_la",
-                },
-            ],
-            "name": "DC_0030_00",
-            "unicodes": [],
-            "sources": [
-                {
-                    "name": "<default>",
-                    "location": {},
-                    "layerName": "foreground",
-                },
-                {
-                    "name": "X_X_bo",
-                    "location": {"X_X_bo": 1.0},
-                    "layerName": "X_X_bo_1_layer",
-                },
-                {
-                    "name": "X_X_la",
-                    "location": {"X_X_la": 1.0},
-                    "layerName": "X_X_la_2_layer",
-                },
-            ],
-            "layers": [
-                {
-                    "name": "foreground",
-                    "glyph": {
-                        "components": [
-                            {
-                                "location": {"WDTH": 0.33, "WGHT": 0.45},
-                                "name": "zero_00",
-                                "transformation": {
-                                    "rotation": 0,
-                                    "scalex": 1,
-                                    "scaley": 1,
-                                    "tcenterx": 0,
-                                    "tcentery": 0,
-                                    "x": 0,
-                                    "y": 0,
-                                },
-                            }
-                        ],
-                        "xAdvance": 600,
-                    },
-                },
-                {
-                    "name": "X_X_bo_1_layer",
-                    "glyph": {
-                        "components": [
-                            {
-                                "location": {"WDTH": 0.33, "WGHT": 1.0},
-                                "name": "zero_00",
-                                "transformation": {
-                                    "rotation": 0,
-                                    "scalex": 1,
-                                    "scaley": 1,
-                                    "tcenterx": 0,
-                                    "tcentery": 0,
-                                    "x": 0,
-                                    "y": 0,
-                                },
-                            }
-                        ],
-                        "xAdvance": 600,
-                    },
-                },
-                {
-                    "name": "X_X_la_2_layer",
-                    "glyph": {
-                        "components": [
-                            {
-                                "location": {"WDTH": 1.0, "WGHT": 0.45},
-                                "name": "zero_00",
-                                "transformation": {
-                                    "rotation": 0,
-                                    "scalex": 1,
-                                    "scaley": 1,
-                                    "tcenterx": 0,
-                                    "tcentery": 0,
-                                    "x": 0,
-                                    "y": 0,
-                                },
-                            }
-                        ],
-                        "xAdvance": 600,
-                    },
-                },
-            ],
-        },
-    ),
 ]
 
 
 testFontPaths = {
-    "rcjk": dataDir / "figArnaud.rcjk",
     "designspace": dataDir / "mutatorsans" / "MutatorSans.designspace",
     "ufo": dataDir / "mutatorsans" / "MutatorSansLightCondensed.ufo",
 }
@@ -649,7 +353,6 @@ def getTestFont(backendName):
 
 
 getGlyphNamesTestData = [
-    ("rcjk", 80, ["DC_0030_00", "DC_0031_00", "DC_0032_00", "DC_0033_00"]),
     ("designspace", 49, ["A", "Aacute", "Adieresis", "B"]),
     ("ufo", 49, ["A", "Aacute", "Adieresis", "B"]),
 ]
@@ -668,7 +371,6 @@ async def test_getGlyphNames(backendName, numGlyphs, firstFourGlyphNames):
 
 
 getReverseCmapTestData = [
-    ("rcjk", 80, {"uni0031": [ord("1")]}),
     ("designspace", 49, {"A": [ord("A")], "B": [ord("B")]}),
     ("ufo", 49, {"A": [ord("A")], "B": [ord("B")]}),
 ]
@@ -695,18 +397,6 @@ async def test_getGlyph(backendName, expectedGlyph):
 
 
 getGlobalAxesTestData = [
-    (
-        "rcjk",
-        [
-            {
-                "defaultValue": 400,
-                "maxValue": 700,
-                "minValue": 400,
-                "label": "Weight",
-                "name": "wght",
-            },
-        ],
-    ),
     (
         "designspace",
         [
@@ -735,7 +425,6 @@ async def test_getGlobalAxes(backendName, expectedGlobalAxes):
 getLibTestData = [
     ("designspace", 0),
     ("ufo", 17),
-    ("rcjk", 4),
 ]
 
 
