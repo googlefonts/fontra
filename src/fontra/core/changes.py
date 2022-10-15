636ef6c61b395165c92e89f07bf87b11d9d0b498
Just van Rossum <justvanrossum@gmail.com>
2022-10-15 23:48:52 +0200

    Attempt to fix/implement casting of json objects to dataclasses as list items

--- src/fontra/core/changes.py
+++ src/fontra/core/changes.py
@@ -1,4 +1,12 @@
-def setItem(subject, key, item):
+from dataclasses import is_dataclass
+from functools import partial
+from typing import get_args
+from .classes import from_dict
+
+
+def setItem(subject, key, item, *, itemCast=None):
+    if itemCast is not None:
+        item = itemCast(item)
     if isinstance(subject, (dict, list)):
         subject[key] = item
     else:
@@ -9,11 +17,13 @@ def delItems(subject, index, deleteCount=1):
     spliceItems(subject, index, deleteCount)
 
 
-def insertItems(subject, index, *items):
-    spliceItems(subject, index, 0, *items)
+def insertItems(subject, index, *items, itemCast=None):
+    spliceItems(subject, index, 0, *items, itemCast=itemCast)
 
 
-def spliceItems(subject, index, deleteCount, *items):
+def spliceItems(subject, index, deleteCount, *items, itemCast=None):
+    if itemCast is not None:
+        items = [itemCast(item) for item in items]
     subject[index : index + deleteCount] = items
 
 
@@ -42,21 +52,43 @@ baseChangeFunctions = {
 #
 
 
-def applyChange(subject, change, changeFunctions):
+def applyChange(subject, change, changeFunctions, itemCast=None):
     path = change.get("p", [])
     functionName = change.get("f")
     children = change.get("c", [])
 
     for pathElement in path:
+        itemCast = None
         if isinstance(subject, (dict, list, tuple)):
             subject = subject[pathElement]
         else:
+            if is_dataclass(subject):
+                itemCast = getItemCast(subject, pathElement)
             subject = getattr(subject, pathElement)
 
     if functionName is not None:
         changeFunc = changeFunctions[functionName]
         args = change.get("a", [])
-        changeFunc(subject, *args)
+        if (
+            itemCast is not None
+            and changeFunc.__kwdefaults__ is not None
+            and "itemCast" in changeFunc.__kwdefaults__
+        ):
+            changeFunc(subject, *args, itemCast=itemCast)
+        else:
+            changeFunc(subject, *args)
 
     for subChange in children:
-        applyChange(subject, subChange, changeFunctions)
+        applyChange(subject, subChange, changeFunctions, itemCast=itemCast)
+
+
+def getItemCast(subject, attrName):
+    # Poking into the bowels of a dataclass. dataclasses.fields returns a tuple :(
+    childType = subject.__class__.__dataclass_fields__[attrName].type
+    if not is_dataclass(childType):
+        # Extract the type for a typed list
+        assert childType.__name__ == "list"
+        args = get_args(childType)
+        if len(args) == 1 and is_dataclass(args[0]):
+            return partial(from_dict, args[0])
+    return None
