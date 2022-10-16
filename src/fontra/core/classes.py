62156812679ca8934dc69f68d8af4ea0d306f698
Just van Rossum <justvanrossum@gmail.com>
2022-10-16 16:20:02 +0200

    create a schema from dataclass definitions

--- src/fontra/core/classes.py
+++ src/fontra/core/classes.py
@@ -1,6 +1,9 @@
-from dataclasses import dataclass, field
+from __future__ import annotations
+
+from dataclasses import dataclass, field, fields, is_dataclass
 from functools import partial
-from typing import Optional
+from typing import Optional, get_args, get_type_hints
+import sys
 import dacite
 from .packedpath import PackedPath, PointType
 
@@ -77,5 +80,57 @@ class GlobalAxis:
     mapping: list[tuple[int, int]] = field(default_factory=list)
 
 
+def makeSchema(*classes, schema=None):
+    if schema is None:
+        schema = {}
+    for cls in classes:
+        if cls in schema:
+            continue
+        cls_globals = vars(sys.modules[cls.__module__])
+        classFields = {}
+        schema[cls] = classFields
+        for name, tp in get_type_hints(cls, cls_globals).items():
+            fieldDef = dict(type=tp)
+            if is_dataclass(tp):
+                makeSchema(tp, schema=schema)
+            elif tp.__name__ == "Optional":
+                [subtype, _] = get_args(tp)
+                fieldDef["type"] = subtype
+                fieldDef["optional"] = True
+                if is_dataclass(subtype):
+                    makeSchema(subtype, schema=schema)
+            elif tp.__name__ == "list":
+                [subtype] = get_args(tp)
+                fieldDef["subtype"] = subtype
+                if is_dataclass(subtype):
+                    makeSchema(subtype, schema=schema)
+            classFields[name] = fieldDef
+    return schema
+
+
+def classesToStrings(schema):
+    return {
+        cls.__name__: {
+            fieldName: {
+                k: v.__name__ if hasattr(v, "__name__") else v
+                for k, v in fieldDef.items()
+            }
+            for fieldName, fieldDef in classFields.items()
+        }
+        for cls, classFields in schema.items()
+    }
+
+
+classesSchema = makeSchema(VariableGlyph)
+
+
 _castConfig = dacite.Config(cast=[PointType])
 from_dict = partial(dacite.from_dict, config=_castConfig)
+
+
+if __name__ == "__main__":
+    import json
+
+    schema = classesToStrings(classesSchema)
+    print("// This file is generated, don't edit!")
+    print("const classesSchema =", json.dumps(schema, indent=2))
