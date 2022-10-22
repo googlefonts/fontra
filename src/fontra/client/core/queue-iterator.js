3157c065b41fb035addd444e505385a53f935f8e
Just van Rossum <justvanrossum@gmail.com>
2022-10-22 15:21:02 +0200

    Add generic async queue iterator

---
+++ src/fontra/client/core/queue-iterator.js
@@ -0,0 +1,53 @@
+export class QueueIterator {
+
+  constructor(maxQueueSize = 10) {
+    this._done = false;
+    this._queue = [];
+    this._maxQueueSize = maxQueueSize;
+    this._signal = null;
+  }
+
+  put(item) {
+    if (this._queue.length >= this._maxQueueSize) {
+      throw new Error("can't put item: queue is full");
+    }
+    if (this._done) {
+      throw new Error("can't put item: queue is done");
+    }
+    this._queue.push(item);
+    this._signal?.();
+  }
+
+  done() {
+    this._done = true;
+    this._signal?.();
+  }
+
+  isDone() {
+    return this._done;
+  }
+
+  [Symbol.asyncIterator]() {
+    return this;
+  }
+
+  next() {
+    if (this._queue.length) {
+      return Promise.resolve({"value": this._queue.shift(), "done": false})
+    } else if (this._done) {
+      return Promise.resolve({"done": true})
+    } else {
+      return new Promise(resolve => {
+        this._signal = () => {
+          if (this._queue.length) {
+            resolve({"value": this._queue.shift(), "done": false});
+          } else {
+            resolve({"done": true})
+          }
+          this._signal = null;
+        }
+      });
+    }
+  }
+
+}
