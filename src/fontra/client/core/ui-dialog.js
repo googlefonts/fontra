da5329d6797c82921ebc2f2608baa82b419122f4
Just van Rossum <justvanrossum@gmail.com>
2022-11-11 14:22:16 +0100

    Add ui-dialog, a simple dialog overlay

---
+++ src/fontra/client/core/ui-dialog.js
@@ -0,0 +1,79 @@
+import { enumerate } from "./utils.js";
+
+
+export function dialog(headline, message, buttonDefs) {
+  /* return a Promise the the result of the user action, or null for cancel */
+
+  const dialogDone = result => {
+    container.innerHTML = "";
+    container.classList.remove("visible");
+    currentActiveElement.focus();
+    resolveDialogResult(result);
+  }
+
+  let resolveDialogResult;
+  const resultPromise = new Promise(resolve => {
+    resolveDialogResult = resolve;
+  })
+
+  const currentActiveElement = document.activeElement;
+
+  const container = document.querySelector("#ui-dialog-container");
+  const content = document.createElement("div");
+  content.className = "ui-dialog-content";
+  content.tabIndex = 1;  /* so we can receive key events */
+  container.appendChild(content);
+
+  const headlineElement = document.createElement("div");
+  headlineElement.classList.add("ui-dialog-headline");
+  headlineElement.innerText = headline
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("ui-dialog-message");
+  messageElement.innerText = message;
+
+  content.appendChild(headlineElement);
+  content.appendChild(messageElement);
+
+  let defaultButtonElement, cancelButtonElement;
+  buttonDefs = buttonDefs.map(bd => {return {...bd};});
+  if (buttonDefs.length === 1) {
+    buttonDefs[0].isDefault = true;
+  }
+  for (const [buttonIndex, buttonDef] of enumerate(buttonDefs, 4 - buttonDefs.length)) {
+    const buttonElement = document.createElement("input");
+    buttonElement.type = "button"
+    buttonElement.className = `ui-dialog-button button-${buttonIndex}`;
+    if (buttonDef.isDefault) {
+      buttonElement.classList.add("default");
+      defaultButtonElement = buttonElement;
+    }
+    buttonElement.value = buttonDef.title;
+    buttonElement.onclick = event => {
+      dialogDone(buttonDef.resultValue || buttonDef.title);
+    }
+    content.appendChild(buttonElement);
+  }
+
+  content.onkeydown = event => {
+    if (event.key == "Enter") {
+      defaultButtonElement?.click();
+    } else if (event.key == "Escape") {
+      cancelButtonElement?.click();
+      if (!cancelButtonElement) {
+        dialogDone(null);
+      }
+    }
+    event.stopImmediatePropagation();
+  };
+
+  /* prevent clicks on the dialog itself to propagate to the container */
+  content.onclick = event => event.stopImmediatePropagation();
+
+  container.classList.add("visible");
+  content.focus();
+  container.onclick = event => {
+    dialogDone(null);
+  }
+
+  return resultPromise;
+}
