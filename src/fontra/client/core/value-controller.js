abf2f7dd225ac387c2bb94bc02d78d585d1c869d
Just van Rossum <justvanrossum@gmail.com>
2022-11-21 08:40:29 +0100

    rewrite ValueController to use callbacks instead of async queue

--- src/fontra/client/core/value-controller.js
+++ src/fontra/client/core/value-controller.js
@@ -1,41 +1,39 @@
-import { QueueIterator } from "./queue-iterator.js";
-
-
 export class ValueController {
 
-  constructor(initialValue) {
+  constructor() {
     this._observers = new Map();
-    this._value = initialValue;
   }
 
-  set(value, senderID) {
-    if (senderID && !this._observers.has(senderID)) {
-      throw new Error("can't set value: unknown observer senderID");
+  get value() {
+    return this._value;
+  }
+
+  set(value, observerID) {
+    if (observerID && !this._observers.has(observerID)) {
+      throw new Error("can't set value: unknown observerID");
     }
     this._value = value;
-    this._observers.forEach((valueStream, valueSenderID, map) => {
-      if (valueSenderID !== senderID) {
-        valueStream.put(value);
+    this._observers.forEach((valueCallback, valueObserverID, map) => {
+      if (valueObserverID !== observerID) {
+        // Don't execute immediately
+        setTimeout(() => valueCallback(value), 0);
       }
     });
 
   }
 
-  observe(senderID) {
-    if (!senderID) {
-      throw new Error("missing/falsey senderID argument");
+  addObserver(observerID, valueCallback) {
+    if (!observerID) {
+      throw new Error("missing/falsey observerID argument");
     }
-    if (this._observers.has(senderID)) {
-      throw new Error("senderID must be unique");
+    if (this._observers.has(observerID)) {
+      throw new Error("observerID must be unique");
     }
-    const valueStream = new QueueIterator();
-    valueStream.put(this._value);
-    this._observers.set(senderID, valueStream);
-    return valueStream;
+    this._observers.set(observerID, valueCallback);
   }
 
-  unobserve(senderID) {
-    this._observers.delete(senderID);
+  removeObserver(observerID) {
+    this._observers.delete(observerID);
   }
 
 }
--- test-js/test-value-controller.js
+++ test-js/test-value-controller.js
@@ -8,58 +8,52 @@ import { ValueController } from "../src/fontra/client/core/value-controller.js";
 describe("ValueController Tests", () => {
 
   it("basic tests", async () => {
-    const vc = new ValueController(1);
-    const obs1 = vc.observe("obs1");
-    const obs2 = vc.observe("obs2");
-    const obs3 = vc.observe("obs3", true);
-    vc.set(100, "obs1");
-    vc.set(200, "obs2");
-    vc.set(300);
+    const vc = new ValueController();
     const result1 = [];
     const result2 = [];
     const result3 = [];
-    collectResult(obs1, result1);
-    collectResult(obs2, result2);
-    collectResult(obs3, result3);
-    await asyncTimeout(0);  // Give the event loop time to handle the events
-    expect(result1).to.deep.equal([1, 200, 300]);
-    expect(result2).to.deep.equal([1, 100, 300]);
-    expect(result3).to.deep.equal([1, 100, 200, 300]);
+    vc.addObserver("obs1", value => result1.push(value));
+    vc.addObserver("obs2", value => result2.push(value));
+    vc.addObserver("obs3", value => result3.push(value));
+    vc.set(100, "obs1");
 
-    vc.unobserve("obs1");
+    expect(vc.value).to.equal(100);
+    expect(() => vc.value = 123).to.throw("Cannot set property value");
+
+    vc.set(200, "obs2");
+    vc.set(300);
+    await asyncTimeout(0);  // Give the event loop time to handle the events
+    expect(result1).to.deep.equal([200, 300]);
+    expect(result2).to.deep.equal([100, 300]);
+    expect(result3).to.deep.equal([100, 200, 300]);
+
+    vc.removeObserver("obs1");
     vc.set(20, "obs2");
     vc.set(30);
     await asyncTimeout(0);  // Give the event loop time to handle the events
 
-    expect(result1).to.deep.equal([1, 200, 300]);
-    expect(result2).to.deep.equal([1, 100, 300, 30]);
-    expect(result3).to.deep.equal([1, 100, 200, 300, 20, 30]);
+    expect(result1).to.deep.equal([200, 300]);
+    expect(result2).to.deep.equal([100, 300, 30]);
+    expect(result3).to.deep.equal([100, 200, 300, 20, 30]);
 
-    expect(() => vc.set(10, "obs1")).to.throw("can't set value: unknown observer senderID");
+    expect(() => vc.set(10, "obs1")).to.throw("can't set value: unknown observerID");
   });
 
-  it("falsey senderID throw test", async () => {
-    const vc = new ValueController(1);
-    expect(() => vc.observe()).to.throw("missing/falsey senderID argument");
-    expect(() => vc.observe(null)).to.throw("missing/falsey senderID argument");
-    expect(() => vc.observe(undefined)).to.throw("missing/falsey senderID argument");
-    expect(() => vc.observe("")).to.throw("missing/falsey senderID argument");
+  it("falsey observerID throw test", async () => {
+    const vc = new ValueController();
+    expect(() => vc.addObserver()).to.throw("missing/falsey observerID argument");
+    expect(() => vc.addObserver(null)).to.throw("missing/falsey observerID argument");
+    expect(() => vc.addObserver(undefined)).to.throw("missing/falsey observerID argument");
+    expect(() => vc.addObserver("")).to.throw("missing/falsey observerID argument");
   });
 
   it("double observe throw test", async () => {
     const vc = new ValueController(1);
-    const obs1 = vc.observe("obs1");
-    expect(() => vc.observe("obs1")).to.throw("senderID must be unique");
+    const obs1 = vc.addObserver("obs1");
+    expect(() => vc.addObserver("obs1")).to.throw("observerID must be unique");
   });
 
 });
-
-
-async function collectResult(observer, result) {
-  for await (const v of observer) {
-    result.push(v);
-  }
-};
 
 
 function asyncTimeout(ms) {
