36e0fc97899dcca88ea648465db4545b0be70150
Just van Rossum <justvanrossum@gmail.com>
2023-01-06 11:29:45 +0100

    Add cmap wrapper, that keeps a reverse mapping up-to-date

---
+++ src/fontra/client/core/cmap.js
@@ -0,0 +1,73 @@
+export function getCmapWrapper(cmap, revCmap) {
+
+  const handler = {
+    set(cmap, prop, value) {
+      const existingValue = cmap[prop];
+      cmap[prop] = value;
+      if (!isNaN(prop)) {
+        const codepoint = parseInt(prop);
+        if (existingValue) {
+          removeReverseMapping(revCmap, existingValue, codepoint);
+        }
+        if (revCmap[value]) {
+          arrayInsertSortedItem(revCmap[value], codepoint);
+        } else {
+          revCmap[value] = [codepoint];
+        }
+      }
+      return true;
+    },
+
+    get(cmap, prop) {
+      return cmap[prop];
+    },
+
+    deleteProperty(cmap, prop) {
+      const existingValue = cmap[prop];
+      delete cmap[prop];
+      if (!isNaN(prop)) {
+        const codepoint = parseInt(prop);
+        if (existingValue) {
+          removeReverseMapping(revCmap, existingValue, codepoint);
+        }
+      }
+      return true;
+    }
+  }
+
+  return new Proxy(cmap, handler);
+}
+
+
+function removeReverseMapping(revCmap, glyphName, codepoint) {
+  const unicodes = revCmap[glyphName];
+  if (!unicodes) {
+    return;
+  }
+  arrayDiscardItem(unicodes, codepoint);
+  if (!unicodes.length) {
+    delete revCmap[glyphName];
+  }
+}
+
+
+function arrayDiscardItem(array, item) {
+  // Remove `item` from `array` if present
+  const index = array.indexOf(item);
+  if (index >= 0) {
+    array.splice(index, 1);
+  }
+}
+
+
+function arrayInsertSortedItem(array, item) {
+  // Insert integer `item` into the sorted `array`, maintaining the sorted order
+  // These arrays are generally very short, no need to bisect
+  for (let index = 0; index < array.length; index++) {
+    if (item < array[index]) {
+      array.splice(index, 0, item);
+      return;
+    }
+  }
+  array.push(item);
+}
---
+++ test-js/test-cmap.js
@@ -0,0 +1,64 @@
+import chai from "chai";
+const expect = chai.expect;
+
+import { getCmapWrapper } from "../src/fontra/client/core/cmap.js";
+
+
+describe("getCmapWrapper tests", () => {
+
+  it("getCmapWrapper add items", () => {
+    const cmapData = {};
+    const revCmap = {};
+    const cmap = getCmapWrapper(cmapData, revCmap);
+
+    cmap[32] = "space";
+    cmap[33] = "double";
+    cmap[34] = "double";
+    expect(cmapData).to.deep.equal({"32": "space", "33": "double", "34": "double"});
+    expect(revCmap).to.deep.equal({"space": [32], "double": [33, 34]});
+  });
+
+  it("getCmapWrapper replace items", () => {
+    const cmapData = {"32": "space", "33": "double", "34": "double"};
+    const revCmap = {"space": [32], "double": [33, 34]};
+    const cmap = getCmapWrapper(cmapData, revCmap);
+
+    cmap[32] = "spacey";
+    cmap[34] = "doubly";
+    expect(cmapData).to.deep.equal({"32": "spacey", "33": "double", "34": "doubly"});
+    expect(revCmap).to.deep.equal({"spacey": [32], "double": [33], "doubly": [34]});
+  });
+
+  it("getCmapWrapper delete items", () => {
+    const cmapData = {"32": "space", "33": "double", "34": "double"};
+    const revCmap = {"space": [32], "double": [33, 34]};
+    const cmap = getCmapWrapper(cmapData, revCmap);
+
+    delete cmap[32];
+    expect(cmapData).to.deep.equal({"33": "double", "34": "double"});
+    expect(revCmap).to.deep.equal({"double": [33, 34]});
+
+    delete cmap[33];
+    expect(cmapData).to.deep.equal({"34": "double"});
+    expect(revCmap).to.deep.equal({"double": [34]});
+
+    delete cmap[34];
+    expect(cmapData).to.deep.equal({});
+    expect(revCmap).to.deep.equal({});
+  });
+
+  it("getCmapWrapper add items, ensure sorted", () => {
+    const cmapData = {};
+    const revCmap = {};
+    const cmap = getCmapWrapper(cmapData, revCmap);
+
+    cmap[35] = "double";
+    expect(revCmap).to.deep.equal({"double": [35]});
+    cmap[33] = "double";
+    expect(revCmap).to.deep.equal({"double": [33, 35]});
+    cmap[34] = "double";
+    expect(revCmap).to.deep.equal({"double": [33, 34, 35]});
+    expect(cmapData).to.deep.equal({"33": "double", "34": "double", "35": "double"});
+  });
+
+});
