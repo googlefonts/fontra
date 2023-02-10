eea7b8c59f9fe017c3b494608bfb65142d0bb47f
Just van Rossum <justvanrossum@gmail.com>
2023-02-10 17:07:38 +0100

    new module for path modifying functions

---
+++ src/fontra/client/core/path-functions.js
@@ -0,0 +1,67 @@
+import { reversed } from "../core/utils.js";
+
+export function splitPathAtPointIndices(path, pointIndices) {
+  let numSplits = 0;
+  const selectionByContour = new Map();
+  for (const pointIndex of pointIndices) {
+    const [contourIndex, contourPointIndex] = path.getContourAndPointIndex(pointIndex);
+    if (!selectionByContour.has(contourIndex)) {
+      selectionByContour.set(contourIndex, []);
+    }
+    selectionByContour.get(contourIndex).push(contourPointIndex);
+  }
+  const selectedContours = [...selectionByContour.keys()];
+  selectedContours.sort((a, b) => b - a); // Reversed!
+
+  for (const contourIndex of selectedContours) {
+    const contour = path.getUnpackedContour(contourIndex);
+    const points = contour.points;
+    // Filter out off-curve points
+    const contourPointIndices = selectionByContour
+      .get(contourIndex)
+      .filter((i) => !points[i].type);
+    if (!contourPointIndices.length) {
+      continue;
+    }
+    numSplits += contourPointIndices.length;
+
+    const pointArrays = [points];
+    let pointIndexBias = 0;
+    if (path.contourInfo[contourIndex].isClosed) {
+      const splitPointIndex = contourPointIndices.pop();
+      pointArrays[0] = splitClosedPointsArray(points, splitPointIndex);
+      pointIndexBias = points.length - splitPointIndex;
+    }
+
+    for (const splitPointIndex of reversed(contourPointIndices)) {
+      const points = pointArrays.pop();
+      const [points1, points2] = splitOpenPointsArray(
+        points,
+        splitPointIndex + pointIndexBias
+      );
+      pointArrays.push(points2);
+      pointArrays.push(points1);
+    }
+
+    path.deleteContour(contourIndex);
+    // Insert the split contours in reverse order
+    for (const points of pointArrays) {
+      // Ensure the end points are not smooth
+      delete points[0].smooth;
+      delete points[points.length - 1].smooth;
+      path.insertUnpackedContour(contourIndex, { points: points, isClosed: false });
+    }
+  }
+  return numSplits;
+}
+
+function splitClosedPointsArray(points, splitPointIndex) {
+  return points.slice(splitPointIndex).concat(points.slice(0, splitPointIndex + 1));
+}
+
+function splitOpenPointsArray(points, splitPointIndex) {
+  if (!splitPointIndex || splitPointIndex >= points.length - 1) {
+    throw new Error(`assert -- invalid point index ${splitPointIndex}`);
+  }
+  return [points.slice(0, splitPointIndex + 1), points.slice(splitPointIndex)];
+}
