40da084abf23ccf121b4cd97d0ed9c969b32198f
Just van Rossum <justvanrossum@gmail.com>
2023-02-12 08:09:44 +0100

    Test multiple contours + cubic

--- test-js/test-path-hit-tester.js
+++ test-js/test-path-hit-tester.js
@@ -35,6 +35,35 @@ describe("PathHitTester Tests", () => {
         y: 49.1,
       },
     ],
+    [
+      { x: 10, y: 210 },
+      20,
+      { contourIndex: 1, segmentIndex: 0, d: 10, t: 0.1, x: 0, y: 210 },
+    ],
+    [
+      { x: 100, y: 210 },
+      20,
+      {
+        contourIndex: 1,
+        segmentIndex: 3,
+        d: 10,
+        t: 0.5,
+        x: 100,
+        y: 200,
+      },
+    ],
+    [
+      { x: 220, y: 250 },
+      20,
+      {
+        contourIndex: 1,
+        segmentIndex: 2,
+        d: 12.956840321996932,
+        t: 0.335,
+        x: 226.73300000000003,
+        y: 238.92993125000007,
+      },
+    ],
   ];
   for (let i = 0; i < hitTest_testData.length; i++) {
     const [testPoint, margin, expectedHit] = hitTest_testData[i];
@@ -44,6 +73,11 @@ describe("PathHitTester Tests", () => {
       p.lineTo(0, 100);
       p.lineTo(200, 100);
       p.quadraticCurveTo(250, 50, 200, 0);
+      p.closePath();
+      p.moveTo(0, 200);
+      p.lineTo(0, 300);
+      p.lineTo(200, 200);
+      p.cubicCurveTo(240, 275, 240, 225, 200, 200);
       p.closePath();
       const pcf = new PathHitTester(p);
       const hit = pcf.hitTest(testPoint, margin);
