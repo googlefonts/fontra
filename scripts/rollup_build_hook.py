b9c7f49eed3b97b0fb7e7d7cb18d905e7ff33f8b
nedkamburov <nedislav.kamburov@gmail.com>
2023-03-21 14:29:08 +0200

    add the right commands for the rollup build hook

--- scripts/rollup_build_hook.py
+++ scripts/rollup_build_hook.py
@@ -5,4 +5,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class RollupBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
+        subprocess.check_output("npm install", shell=True)
         subprocess.check_output("npm run bundle-rollup", shell=True)
