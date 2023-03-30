bb89141468deff2743b4d594cf4d1ace937a9146
Just van Rossum <justvanrossum@gmail.com>
2023-03-30 14:42:07 +0200

    Move func to the top

--- src/fontra/client/core/theme-settings.js
+++ src/fontra/client/core/theme-settings.js
@@ -1,5 +1,14 @@
 import { newObservableObject } from "./observable-object.js";
 
+function setupThemeOverride(value) {
+  const rootElement = document.querySelector("html");
+  rootElement.classList.remove("light-theme");
+  rootElement.classList.remove("dark-theme");
+  if (value !== "automatic") {
+    rootElement.classList.add(value + "-theme");
+  }
+}
+
 export const themeModelObject = newObservableObject({ theme: "automatic" });
 
 themeModelObject.synchronizeWithLocalStorage("fontra-");
@@ -11,12 +20,3 @@ themeModelObject.addEventListener("changed", (event) => {
 });
 
 setupThemeOverride(themeModelObject.theme);
-
-function setupThemeOverride(value) {
-  const rootElement = document.querySelector("html");
-  rootElement.classList.remove("light-theme");
-  rootElement.classList.remove("dark-theme");
-  if (value !== "automatic") {
-    rootElement.classList.add(value + "-theme");
-  }
-}
