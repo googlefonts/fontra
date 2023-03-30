0da9530fdf0e278aea06fbdb12d27c567401ac93
Just van Rossum <justvanrossum@gmail.com>
2023-03-30 14:14:19 +0200

    make private and rename helper func

--- src/fontra/client/core/theme-settings.js
+++ src/fontra/client/core/theme-settings.js
@@ -1,23 +1,23 @@
 import { newObservableObject } from "./observable-object.js";
 
-export function themeSwitch(value) {
+export const themeModelObject = newObservableObject({ theme: "automatic" });
+themeModelObject.synchronizeWithLocalStorage("fontra-");
+
+export function themeSwitchFromLocalStorage() {
+  setupThemeOverride(themeModelObject.theme);
+
+  themeModelObject.addEventListener("changed", (event) => {
+    if (event.key === "theme") {
+      setupThemeOverride(themeModelObject.theme);
+    }
+  });
+}
+
+function setupThemeOverride(value) {
   const rootElement = document.querySelector("html");
   rootElement.classList.remove("light-theme");
   rootElement.classList.remove("dark-theme");
   if (value !== "automatic") {
     rootElement.classList.add(value + "-theme");
   }
-}
-
-export const themeModelObject = newObservableObject({ theme: "automatic" });
-themeModelObject.synchronizeWithLocalStorage("fontra-");
-
-export function themeSwitchFromLocalStorage() {
-  themeSwitch(themeModelObject.theme);
-
-  themeModelObject.addEventListener("changed", (event) => {
-    if (event.key === "theme") {
-      themeSwitch(themeModelObject.theme);
-    }
-  });
 }
