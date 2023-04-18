fde8c85197510ae2d15fe37abeb6302107ef30f4
Just van Rossum <justvanrossum@gmail.com>
2023-04-18 19:16:59 +0200

    Some tests for var-glyph

---
+++ test-js/test-var-glyph.js
@@ -0,0 +1,74 @@
+import chai from "chai";
+const expect = chai.expect;
+
+import { enumerate } from "../src/fontra/client/core/utils.js";
+import { StaticGlyph, VariableGlyph } from "../src/fontra/client/core/var-glyph.js";
+
+function makeTestGlyphObject() {
+  return {
+    name: "a",
+    axes: [],
+    sources: [{ name: "default", layerName: "default", location: {} }],
+    layers: [
+      {
+        name: "default",
+        glyph: {
+          xAdvance: 500,
+          yAdvance: 1000,
+          verticalOrigin: 800,
+          path: { contourInfo: [], coordinates: [], pointTypes: [] },
+          components: [
+            { name: "test", location: { a: 0.5 }, transformation: { translateX: 0 } },
+          ],
+        },
+      },
+    ],
+  };
+}
+
+describe("var-glyph Tests", () => {
+  it("new VariableGlyph", () => {
+    const vgObj = makeTestGlyphObject();
+    const vg = VariableGlyph.fromObject(vgObj);
+    expect(vg).to.deep.equal(vgObj);
+  });
+
+  const modifierFuncs = [
+    (vg) => vg.axes.push({ name: "wght" }),
+    (vg) => (vg.axes = [{ name: "wght" }]),
+    (vg) => (vg.sources[0].location.x = 123),
+    (vg) => (vg.layers[0].glyph.xAdvance = 501),
+    (vg) => vg.layers[0].glyph.path.pointTypes.push(0),
+    (vg) => (vg.layers[0].glyph.path.pointTypes = [0]),
+    (vg) => vg.layers[0].glyph.path.coordinates.push(0, 0),
+    (vg) => (vg.layers[0].glyph.path.coordinates = [0, 0]),
+    (vg) => vg.layers[0].glyph.path.contourInfo.push({}),
+    (vg) => (vg.layers[0].glyph.path.contourInfo = [{}]),
+    (vg) => (vg.layers[0].glyph.components[0].name = "test2"),
+    (vg) => (vg.layers[0].glyph.components[0].location.x = 2),
+    (vg) => (vg.layers[0].glyph.components[0].transformation.translateX = 2),
+  ];
+
+  for (const [i, m] of enumerate(modifierFuncs)) {
+    it(`modify VariableGlyph ${i}`, () => {
+      const vgObj = makeTestGlyphObject();
+      const vg = VariableGlyph.fromObject(vgObj);
+      m(vg);
+      expect(vg).to.not.deep.equal(vgObj);
+    });
+  }
+
+  for (const [i, m] of enumerate(modifierFuncs)) {
+    it(`copy + modify VariableGlyph ${i}`, () => {
+      const vgObj = makeTestGlyphObject();
+      const vg = VariableGlyph.fromObject(vgObj);
+      const vg2 = vg.copy();
+      m(vg2);
+      expect(vg2).to.not.deep.equal(vg);
+    });
+  }
+});
+
+function copyObject(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
