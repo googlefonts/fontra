acb9cb28fa47914e237f3a768d2004fe204a36c8
Just van Rossum <justvanrossum@gmail.com>
2023-06-09 16:11:18 +0200

    Delete layer from UFO glyphset if it was deleted in Fontra

--- src/fontra/backends/designspace.py
+++ src/fontra/backends/designspace.py
@@ -207,9 +207,11 @@ class DesignspaceBackend:
                 )
             layerNameMapping[source.layerName] = globalSource["layerName"]
 
+        usedLayers = set()
         for layerName, layer in glyph.layers.items():
             layerName = layerNameMapping.get(layerName, layerName)
             glyphSet = self.ufoGlyphSets[layerName]
+            usedLayers.add(layerName)
             writeGlyphSetContents = glyphName not in glyphSet
             layerGlyph, drawPointsFunc = buildUFOLayerGlyph(
                 glyphSet, glyphName, layer.glyph, unicodes
@@ -227,6 +229,18 @@ class DesignspaceBackend:
                 self.updateGlyphSetContents(glyphSet)
 
             modTimes.add(glyphSet.getGLIFModificationTime(glyphName))
+
+        relevantLayerNames = set(
+            ln for ln, gs in self.ufoGlyphSets.items() if glyphName in gs
+        )
+        layersToDelete = relevantLayerNames - usedLayers
+        for layerName in layersToDelete:
+            glyphSet = self.ufoGlyphSets[layerName]
+            glyphSet.deleteGlyph(glyphName)
+            # FIXME: this is inefficient if we write many glyphs
+            self.updateGlyphSetContents(glyphSet)
+            modTimes.add(None)
+
         self.savedGlyphModificationTimes[glyphName] = modTimes
 
     def _findGlobalSource(self, location):
