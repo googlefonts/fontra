d995d03729546cab0f3a1ad42a4471ba95e6ca7a
Just van Rossum <justvanrossum@gmail.com>
2023-06-19 20:19:28 +0200

    Test adding a new source

--- test-py/test_backend_designspace.py
+++ test-py/test_backend_designspace.py
@@ -2,8 +2,10 @@ import pathlib
 import shutil
 
 import pytest
+from fontTools.designspaceLib import DesignSpaceDocument
 
 from fontra.backends.designspace import DesignspaceBackend
+from fontra.core.classes import Layer, Source, StaticGlyph
 
 dataDir = pathlib.Path(__file__).resolve().parent / "data"
 
@@ -42,3 +44,38 @@ async def test_roundTripGlyph(writableTestFont, glyphName):
     for layerName in existingData:
         assert existingData[layerName] == newData[layerName], layerName
     assert existingData == newData  # just in case the keys differ
+
+
+async def test_addNewSource(writableTestFont):
+    glyphName = "A"
+    glyphMap = await writableTestFont.getGlyphMap()
+    glyph = await writableTestFont.getGlyph(glyphName)
+    dsSources = unpackSources(writableTestFont.dsDoc.sources)
+
+    glyph.sources.append(Source(name="mid", location={"weight": 400}, layerName="mid"))
+    glyph.layers["mid"] = Layer(glyph=StaticGlyph())
+
+    await writableTestFont.putGlyph(glyphName, glyph, glyphMap[glyphName])
+
+    newDSDoc = DesignSpaceDocument.fromfile(writableTestFont.dsDoc.path)
+    newDSSources = unpackSources(newDSDoc.sources)
+    assert dsSources == newDSSources[: len(dsSources)]
+    assert len(newDSSources) == len(dsSources) + 1
+    assert newDSSources[-1] == dict(
+        location=dict(weight=400, width=0),
+        styleName="mid",
+        filename="MutatorSansLightCondensed.ufo",
+        layerName="mid",
+    )
+
+
+def unpackSources(sources):
+    return [
+        dict(
+            location=s.location,
+            styleName=s.styleName,
+            filename=s.filename,
+            layerName=s.layerName,
+        )
+        for s in sources
+    ]
