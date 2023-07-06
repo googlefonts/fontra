76c25a9fb0450624df9099711c41143fca522bfd
Just van Rossum <justvanrossum@gmail.com>
2023-07-06 09:55:58 +0200

    Subscribe to current glyph changes

--- src/fontra/views/editor/edit-tools-power-ruler.js
+++ src/fontra/views/editor/edit-tools-power-ruler.js
@@ -4,6 +4,48 @@ export class PowerRulerTool extends BaseTool {
   iconPath = "/images/ruler.svg";
   identifier = "power-ruler-tool";
 
+  constructor(editor) {
+    super(editor);
+    this.fontController = editor.fontController;
+    this.glyphRulers = {};
+    this.currentGlyphName = undefined;
+
+    this.sceneController.addEventListener("selectedGlyphChanged", () =>
+      this.editedGlyphMayHaveChanged()
+    );
+    this.sceneController.addEventListener("selectedGlyphIsEditingChanged", () =>
+      this.editedGlyphMayHaveChanged()
+    );
+
+    this.glyphChangeListener = (glyphName) => this.glyphChanged(glyphName);
+  }
+
+  editedGlyphMayHaveChanged() {
+    const glyphName = this.sceneController.selectedGlyphIsEditing
+      ? this.sceneModel.getSelectedGlyphName()
+      : undefined;
+    if (glyphName !== this.currentGlyphName) {
+      this.editedGlyphChanged(glyphName);
+    }
+  }
+
+  editedGlyphChanged(glyphName) {
+    if (this.currentGlyphName) {
+      this.fontController.removeGlyphChangeListener(
+        this.currentGlyphName,
+        this.glyphChangeListener
+      );
+    }
+    if (glyphName) {
+      this.fontController.addGlyphChangeListener(glyphName, this.glyphChangeListener);
+    }
+    this.currentGlyphName = glyphName;
+  }
+
+  glyphChanged(glyphName) {
+    console.log(glyphName, "changed");
+  }
+
   handleHover(event) {
     this.setCursor();
   }
