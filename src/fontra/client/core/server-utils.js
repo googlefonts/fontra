07748d06eb5ebba00ad0254b7ca383d9e5903fa1
Just van Rossum <justvanrossum@gmail.com>
2023-07-11 10:13:15 +0200

    Remove 'statuc' remote functions from FontController/FontHandler, and create a new server-utils web api mechanism

---
+++ src/fontra/client/core/server-utils.js
@@ -0,0 +1,24 @@
+export async function callServerAPI(functionName, kwargs) {
+  const response = await fetch(`/api/${functionName}`, {
+    method: "POST",
+    body: JSON.stringify(kwargs),
+  });
+
+  const result = await response.json();
+  if (result.error) {
+    throw new Error(result.error);
+  }
+  return result.returnValue;
+}
+
+export async function getSuggestedGlyphName(codePoint) {
+  return await callServerAPI("getSuggestedGlyphName", { codePoint });
+}
+
+export async function getUnicodeFromGlyphName(glyphName) {
+  return await callServerAPI("getUnicodeFromGlyphName", { glyphName });
+}
+
+export async function parseClipboard(data) {
+  return await callServerAPI("parseClipboard", { data });
+}
