ea17f6204ef04127f1ab8f4dc751cca1a74da6fd
Fatih Erikli <fatih.erikli.work@gmail.com>
2023-07-12 11:15:40 +0300

    sectRect should not create a rectangle with sizes of 0

--- test-js/test-rectangle.js
+++ test-js/test-rectangle.js
@@ -129,8 +129,8 @@ describe("sectRect", () => {
     [
       { xMin: 50, yMin: 50, xMax: 60, yMax: 60 },
       { xMin: 60, yMin: 60, xMax: 70, yMax: 70 },
-      { xMin: 60, yMin: 60, xMax: 60, yMax: 60 },
-      "Creates rectangle with sizes of 0 if they intersect at the edge", // should be clarified with @justvanrossum
+      undefined,
+      "Should not create a rectangle with sizes of 0 if they intersect at the edge",
     ],
     [
       { xMin: 50, yMin: 50, xMax: 60, yMax: 60 },
