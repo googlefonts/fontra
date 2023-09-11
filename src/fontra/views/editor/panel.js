f3c4f2598afebbe3e0ca79784788673d976a6af8
Fatih Erikli <fatih.erikli.work@gmail.com>
2023-09-11 18:01:31 +0300

    Refactor into a single module

--- src/fontra/views/editor/panel.js
+++ src/fontra/views/editor/panel.js
@@ -1,9 +1,15 @@
 import { SimpleElement } from "/core/unlit.js";
 
 export default class Panel extends SimpleElement {
-  constructor(editorController) {
+  constructor(editorController, renderContentElement = true) {
     super();
     this.editorController = editorController;
+    if (renderContentElement) {
+      this.renderContentElement();
+    }
+  }
+
+  renderContentElement() {
     this.contentElement = this.getContentElement();
     this.shadowRoot.appendChild(this.contentElement);
   }
