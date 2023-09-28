3762432aba00b5873706a3711eb2729902f33377
Just van Rossum <justvanrossum@gmail.com>
2023-09-28 15:35:43 +0200

    Make Add Component work with deep-edit, eliminat 'Add the component to all sources' checkbox

--- src/fontra/views/editor/editor.js
+++ src/fontra/views/editor/editor.js
@@ -1078,45 +1078,9 @@ export class EditorController {
       { title: "Cancel", isCancelButton: true },
       { title: "Add", isDefaultButton: true, result: "ok", disabled: true },
     ]);
-    const addToAllSourcesLocalStorageKey = "fontra-add-the-component-to-all-sources";
-    let addToAllSources =
-      localStorage.getItem(addToAllSourcesLocalStorageKey) === "true";
 
-    dialog.setContent(
-      html.div(
-        {
-          style: `
-          grid-row: 2 / -1;
-          display: flex;
-          flex-direction: column;
-          gap: 0.5em;
-        `,
-        },
-        [
-          glyphsSearch,
-          html.div({}, [
-            html.input({
-              type: "checkbox",
-              id: "add-to-all-sources",
-              checked: addToAllSources,
-              onclick: (event) => {
-                addToAllSources = event.target.checked;
-                localStorage.setItem(
-                  addToAllSourcesLocalStorageKey,
-                  addToAllSources ? "true" : "false"
-                );
-              },
-            }),
-            html.label(
-              {
-                for: "add-to-all-sources",
-              },
-              ["Add the component to all sources"]
-            ),
-          ]),
-        ]
-      )
-    );
+    dialog.setContent(glyphsSearch);
+
     setTimeout(() => glyphsSearch.focusSearchField(), 0); // next event loop iteration
 
     if (!(await dialog.run())) {
@@ -1150,33 +1114,19 @@ export class EditorController {
       transformation: transformation,
       location: location,
     };
-    if (addToAllSources) {
-      await this.sceneController.editGlyphAndRecordChanges((glyph) => {
-        const layerNames = new Set();
-        for (const source of glyph.sources) {
-          layerNames.add(source.layerName);
-        }
-        for (const layerName of layerNames) {
-          const layer = glyph.layers[layerName];
-          layer.glyph.components.push({
-            name: newComponent.name,
-            transformation: { ...newComponent.transformation },
-            location: { ...newComponent.location },
-          });
-        }
-        const instance = this.sceneModel.getSelectedPositionedGlyph().glyph.instance;
-        const newComponentIndex = instance.components.length - 1;
-        this.sceneController.selection = new Set([`component/${newComponentIndex}`]);
-        return "Add Component";
-      });
-    } else {
-      await this.sceneController.editInstanceAndRecordChanges((instance) => {
-        const newComponentIndex = instance.components.length;
-        instance.components.push(newComponent);
-        this.sceneController.selection = new Set([`component/${newComponentIndex}`]);
-        return "Add Component";
-      });
-    }
+    await this.sceneController.editLayersAndRecordChanges((layerGlyphs) => {
+      for (const layerGlyph of Object.values(layerGlyphs)) {
+        layerGlyph.components.push({
+          name: newComponent.name,
+          transformation: { ...newComponent.transformation },
+          location: { ...newComponent.location },
+        });
+      }
+      const instance = this.sceneModel.getSelectedPositionedGlyph().glyph.instance;
+      const newComponentIndex = instance.components.length - 1;
+      this.sceneController.selection = new Set([`component/${newComponentIndex}`]);
+      return "Add Component";
+    });
   }
 
   canSelectAllNone(selectNone) {
