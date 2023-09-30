1458622d984412e9cde704e38e813a6733853dd1
Fatih Erikli <fatih.erikli.work@gmail.com>
2023-09-30 17:51:04 +0300

    Add plugins page

---
+++ src/fontra/client/web-components/plugin-manager.js
@@ -0,0 +1,150 @@
+import { ObservableController } from "../core/observable-object.js";
+import * as html from "../core/unlit.js";
+import { SimpleElement } from "../core/unlit.js";
+import { dialog, dialogSetup } from "/web-components/modal-dialog.js";
+import { UIList } from "/web-components/ui-list.js";
+
+export class PluginManager extends SimpleElement {
+  static styles = `
+  .plugin-manager {
+    margin: 1rem;
+    display: grid;
+    grid-auto-rows: auto auto;
+    grid-gap: .5rem;
+  }
+  .buttons {
+    display: flex;
+    gap: 0.2rem;
+  }
+
+  .no-plugins {
+    padding: 0.4rem 0.2rem;
+  }
+  `;
+  constructor() {
+    super();
+    this.contentElement = this.shadowRoot.appendChild(html.div());
+    this.contentElement.classList.add("plugin-manager");
+    const observable = new ObservableController({
+      plugins: [],
+    });
+    observable.synchronizeWithLocalStorage("fontra.plugins");
+    this.observable = observable;
+    this.pluginList = new UIList();
+    this.pluginList.setItems(observable.model.plugins);
+    this.pluginList.columnDescriptions = [
+      {
+        key: "address",
+        title: "Github repository",
+      },
+    ];
+    this.render();
+    this.renderPlugins();
+    this.pluginList.addEventListener("listSelectionChanged", async (event) => {
+      this.deleteButton.style.display =
+        this.pluginList.selectedItemIndex === undefined ? "none" : "unset";
+    });
+  }
+
+  async promptAddPlugin(text = "") {
+    const newPluginPrompt = await dialogSetup(
+      "Provide a github handle/repository",
+      "",
+      [
+        { title: "Cancel", resultValue: "no", isCancelButton: true },
+        { title: "Create", resultValue: "ok", isDefaultButton: true },
+      ]
+    );
+    let address = text;
+    newPluginPrompt.setContent(
+      html.input({
+        autofocus: true,
+        value: text,
+        oninput: (event) => {
+          address = event.target.value;
+        },
+      })
+    );
+    const pluginPromptResult = await newPluginPrompt.run();
+    if (pluginPromptResult === "ok") {
+      const newPlugin = { address };
+      const [ok, errorMessage] = await this.validatePlugin(newPlugin);
+      if (ok) {
+        this.observable.setItem("plugins", [
+          ...this.observable.model.plugins,
+          newPlugin,
+        ]);
+        this.renderPlugins();
+      } else {
+        await dialog("Error", errorMessage, [{ title: "Ok", isDefaultButton: true }]);
+        return this.promptAddPlugin(address);
+      }
+    }
+  }
+
+  async validatePlugin(plugin) {
+    if (!plugin.address.includes("/")) {
+      return [
+        false,
+        "Provide the github repository in 'github-username/repository-name' format.",
+      ];
+    }
+    if (
+      this.observable.model.plugins.some(({ address }) => address === plugin.address)
+    ) {
+      return [false, "Plugin exists."];
+    }
+    return [true];
+  }
+
+  renderPlugins() {
+    const plugins = this.observable.model.plugins;
+    this.pluginList.setItems(plugins);
+    if (plugins.length === 0) {
+      this.pluginsEmptyState.style.display = "unset";
+    } else {
+      this.pluginsEmptyState.style.display = "none";
+    }
+  }
+
+  render() {
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(
+      html.div({ class: "buttons" }, [
+        html.button(
+          {
+            onclick: () => {
+              this.promptAddPlugin();
+            },
+          },
+          ["Add a plugin"]
+        ),
+        (this.deleteButton = html.button(
+          {
+            style: "display: none",
+            onclick: () => {
+              this.observable.setItem(
+                "plugins",
+                this.observable.model.plugins.filter(
+                  (plugin, index) => index !== this.pluginList.getSelectedItemIndex()
+                )
+              );
+              this.renderPlugins();
+            },
+          },
+          ["Delete"]
+        )),
+      ])
+    );
+    fragment.appendChild(this.pluginList);
+    fragment.appendChild(
+      (this.pluginsEmptyState = html.div(
+        { class: "no-plugins", style: "display: none" },
+        ["No plugins added."]
+      ))
+    );
+    this.contentElement.appendChild(fragment);
+  }
+}
+
+customElements.define("plugin-manager", PluginManager);
