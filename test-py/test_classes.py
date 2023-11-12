183392dd476276d4260fcc06284fe2978b8a2982
Just van Rossum <justvanrossum@gmail.com>
2023-11-12 21:05:43 +0100

    Test casting

--- test-py/test_classes.py
+++ test-py/test_classes.py
@@ -1,7 +1,9 @@
 import json
 import pathlib
+from dataclasses import asdict
 
-from fontra.core.classes import serializableClassSchema
+from fontra.backends.fontra import deserializeGlyph
+from fontra.core.classes import VariableGlyph, classCastFuncs, serializableClassSchema
 
 repoRoot = pathlib.Path(__file__).resolve().parent.parent
 jsonPath = repoRoot / "src" / "fontra" / "client" / "core" / "classes.json"
@@ -14,3 +16,18 @@ def test_classes_json():
     assert (
         serializableClassSchema() == classesFromJSON
     ), "classes.json is stale, please run ./scripts/rebuild_classes_json.sh"
+
+
+def test_cast():
+    glyphPath = (
+        repoRoot
+        / "test-common"
+        / "fonts"
+        / "MutatorSans.fontra"
+        / "glyphs"
+        / "B^1.json"
+    )
+    originalGlyph = deserializeGlyph(glyphPath.read_text(encoding="utf-8"))
+    unstructuredGlyph = asdict(originalGlyph)
+    glyph = classCastFuncs[VariableGlyph](unstructuredGlyph)
+    assert glyph == originalGlyph
