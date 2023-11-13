f061339c156d4fa9f316a10fd819fb3fc75aad1c
Just van Rossum <justvanrossum@gmail.com>
2023-11-13 09:14:01 +0100

    Test path conversion and string repr

--- test-py/test_path_io.py
+++ test-py/test_path_io.py
@@ -134,3 +134,33 @@ async def test_unpackPathRoundTrip(path):
     repackedPath = PackedPath.fromUnpackedContours(unpackedPath)
     assert path == repackedPath
     assert cattrs.unstructure(path) == cattrs.unstructure(repackedPath)
+
+
+@pytest.mark.parametrize("path", pathTestData)
+async def test_pathConversion(path):
+    packedPath = cattrs.structure(path, PackedPath)
+    path = packedPath.asPath()
+    packedPath2 = path.asPackedPath()
+    assert packedPath == packedPath2
+
+
+expectedPackedPathRepr = "PackedPath(coordinates=[60, 0, 110, 0, 110, 120, 60, 120], \
+pointTypes=[<PointType.ON_CURVE: 0>, <PointType.ON_CURVE: 0>, <PointType.ON_CURVE: 0>, \
+<PointType.ON_CURVE: 0>], contourInfo=[ContourInfo(endPoint=3, isClosed=True)])"
+
+
+async def test_packedPathRepr():
+    path = pathTestData[0]
+    packedPath = cattrs.structure(path, PackedPath)
+    assert expectedPackedPathRepr == str(packedPath)
+
+
+expectedPathRepr = "Path(contours=[Contour(points=[{'x': 60, 'y': 0}, {'x': 110, 'y': \
+0}, {'x': 110, 'y': 120}, {'x': 60, 'y': 120}], isClosed=True)])"
+
+
+async def test_pathRepr():
+    path = pathTestData[0]
+    packedPath = cattrs.structure(path, PackedPath)
+    path = packedPath.asPath()
+    assert expectedPathRepr == str(path)
