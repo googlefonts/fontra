a5ccfc64f7164e22e81ec450e6b83f4a799a8ac1
Just van Rossum <justvanrossum@gmail.com>
2024-02-13 14:38:03 +0100

    Beginnings of axis/avar mapping visualization

--- src/fontra/views/fontinfo/fontinfo.js
+++ src/fontra/views/fontinfo/fontinfo.js
@@ -4,6 +4,7 @@ import { addStyleSheet } from "../core/html-utils.js";
 import { ObservableController } from "../core/observable-object.js";
 import { getRemoteProxy } from "../core/remote.js";
 import { labeledTextInput, setupSortableList } from "../core/ui-utils.js";
+import { zip } from "../core/utils.js";
 import { makeDisplayPath } from "../core/view-utils.js";
 import { IconButton } from "../web-components/icon-button.js";
 
@@ -147,7 +148,7 @@ addStyleSheet(`
   padding: 1em;
   cursor: pointer;
   display: grid;
-  grid-template-columns: max-content max-content auto;
+  grid-template-columns: max-content max-content max-content auto;
   gap: 0.5em;
 }
 
@@ -207,6 +208,7 @@ function makeAxisBox(axis) {
           )
           .flat()
       ),
+      buildMappingGraph(axis),
       html.createDomElement("icon-button", {
         class: "fontra-ui-font-info-axes-panel-axis-box-delete",
         src: "/tabler-icons/trash.svg",
@@ -214,6 +216,71 @@ function makeAxisBox(axis) {
       }),
     ]
   );
+}
+
+function buildMappingGraph(axis) {
+  if (!axis.mapping.length) {
+    return html.div(); // filler
+  }
+  const marginLeft = 4;
+  const marginRight = 16;
+  const marginTop = 4;
+  const marginBottom = 16;
+  const graphSize = 100;
+  const width = graphSize + marginLeft + marginRight;
+  const height = graphSize + marginTop + marginBottom;
+  const xs = axis.mapping.map(([x, y]) => x);
+  const ys = axis.mapping.map(([x, y]) => y);
+  const xMin = Math.min(...xs);
+  const xMax = Math.max(...xs);
+  const yMin = Math.min(...ys);
+  const yMax = Math.max(...ys);
+  const graphX = xs.map((x) => (graphSize * (x - xMin)) / (xMax - xMin));
+  const graphY = ys.map((y) => (graphSize * (y - yMin)) / (yMax - yMin));
+  const pointsString = [...zip(graphX, graphY)].flat().join(" ");
+  const nodeString = [...zip(xs, ys, graphX, graphY)]
+    .map(
+      ([x, y, gx, gy]) => `<circle class="node" cx="${gx}" cy="${gy}" fill="black" />`
+    )
+    .join("\n");
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}"
+  viewBox="0 0 ${width} ${height}">
+  <style>
+  text {
+    font-size: 0.8em;
+  }
+  .node {
+    r: 4px;
+    transition: 200ms;
+  }
+  .node:hover {
+    r: 6px;
+  }
+  </style>
+  <rect x="0" y="0" width="${width}" height="${height}" fill="#F8F8F8" />
+  <g transform="translate(0, ${height}) scale(1, -1)">
+    <g transform="translate(${marginLeft}, ${marginBottom})">
+      <rect x="0" y="0" width="${graphSize}" height="${graphSize}"
+       fill="none" stroke="#8886"/>
+      <polyline points="${pointsString}" stroke="gray" fill="none"
+       stroke-linecap="round" stroke-linejoin="round" stroke-width="2px"/>
+      ${nodeString}
+      <text x="0" y="10" transform="scale(1, -1)">${xMin}</text>
+      <text x="${
+        graphSize / 2
+      }" y="10" text-anchor="middle" transform="scale(1, -1)">user</text>
+      <text x="${graphSize}" y="10" text-anchor="end" transform="scale(1, -1)">${xMax}</text>
+      <g transform="translate(${graphSize}) rotate(90)">
+        <text x="0" y="10" transform="scale(1, -1)">${yMin}</text>
+        <text x="${
+          graphSize / 2
+        }" y="10" text-anchor="middle" transform="scale(1, -1)">source</text>
+        <text x="${graphSize}" y="10" text-anchor="end" transform="scale(1, -1)">${yMax}</text>
+      </g>
+    </g>
+  </g>
+</svg>`;
+  return html.htmlToElement(svgString);
 }
 
 class SourcesPanel extends BaseInfoPanel {
