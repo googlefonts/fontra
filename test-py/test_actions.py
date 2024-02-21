65de670d766eb5017cfdd4bd226c5e295b740743
Just van Rossum <justvanrossum@gmail.com>
2024-02-21 10:11:25 +0100

    More solid testing

--- test-py/test_actions.py
+++ test-py/test_actions.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import pytest
+from fontTools.misc.arrayTools import scaleRect
 
 from fontra.actions.actions import getAction
 from fontra.backends import getFileSystemBackend
@@ -14,22 +15,32 @@ def testFontraFont():
     return getFileSystemBackend(commonFontsDir / "MutatorSans.fontra")
 
 
-async def test_scaleAction(testFontraFont):
-    unscaledGlyph = await testFontraFont.getGlyph("period")
-    arguments = {"scaleFactor": 2}
-    action = getAction("scale", testFontraFont, arguments)
-    scaledGlyph = await action.getGlyph("period")
+@pytest.mark.parametrize("glyphName", ["A", "Q", "Adieresis", "period"])
+async def test_scaleAction(testFontraFont, glyphName):
+    scaleFactor = 2
 
-    layerName = [*unscaledGlyph.layers.keys()][0]
-    assert unscaledGlyph.layers[layerName].glyph.path.getControlBounds() == (
-        60,
-        0,
-        110,
-        120,
-    )
-    assert scaledGlyph.layers[layerName].glyph.path.getControlBounds() == (
-        120,
-        0,
-        220,
-        240,
-    )
+    unscaledGlyph = await testFontraFont.getGlyph(glyphName)
+    arguments = {"scaleFactor": scaleFactor}
+    action = getAction("scale", testFontraFont, arguments)
+    scaledGlyph = await action.getGlyph(glyphName)
+
+    for unscaledLayer, scaledLayer in zip(
+        unscaledGlyph.layers.values(), scaledGlyph.layers.values()
+    ):
+        unscaledLayerGlyph = unscaledLayer.glyph
+        scaledLayerGlyph = scaledLayer.glyph
+
+        unscaledBounds = unscaledLayerGlyph.path.getControlBounds()
+        scaledBounds = scaledLayerGlyph.path.getControlBounds()
+        if unscaledBounds is None:
+            assert scaledBounds is None
+        else:
+            assert scaleRect(unscaledBounds, scaleFactor, scaleFactor) == scaledBounds
+
+        for unscaledComponent, scaledComponent in zip(
+            unscaledLayerGlyph.components, scaledLayerGlyph.components
+        ):
+            assert (
+                unscaledComponent.transformation.translateX * scaleFactor
+                == scaledComponent.transformation.translateX
+            )
