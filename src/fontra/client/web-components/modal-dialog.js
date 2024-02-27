de3c7ff40071edc3338489b9c4e210afe0cac92f
elevchyt <lehy64@gmail.com>
2024-02-27 20:23:30 +0200

    close dialog via backdrop click

--- src/fontra/client/web-components/modal-dialog.js
+++ src/fontra/client/web-components/modal-dialog.js
@@ -133,6 +133,9 @@ export class ModalDialog extends SimpleElement {
       onkeydown: (event) => this._handleKeyDown(event),
     });
     this.dialogElement = document.createElement("dialog");
+    this.dialogElement.addEventListener("click", (event) => {
+      event.target.nodeName == "DIALOG" ? this._dialogDone(null) : null;
+    });
     this.dialogElement.appendChild(this.dialogBox);
     this.shadowRoot.append(this.dialogElement);
   }
