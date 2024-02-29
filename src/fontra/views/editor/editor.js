795c0e83bc45dc1b69c707876ecd6e9f85ff618e
elevchyt <lehy64@gmail.com>
2024-02-29 19:37:12 +0200

    view menu bar item error fix

--- src/fontra/views/editor/editor.js
+++ src/fontra/views/editor/editor.js
@@ -312,14 +312,16 @@ export class EditorController {
               title: "Zoom to fit",
               enabled: () => {
                 let viewBox = this.sceneController.getSelectionBox();
-                const size = rectSize(viewBox);
-                if (size.width < 4 && size.height < 4) {
-                  const center = rectCenter(viewBox);
-                  viewBox = centeredRect(center.x, center.y, 10, 10);
-                } else {
-                  viewBox = rectAddMargin(viewBox, 0.1);
+                if (viewBox) {
+                  const size = rectSize(viewBox);
+                  if (size.width < 4 && size.height < 4) {
+                    const center = rectCenter(viewBox);
+                    viewBox = centeredRect(center.x, center.y, 10, 10);
+                  } else {
+                    viewBox = rectAddMargin(viewBox, 0.1);
+                  }
+                  return !this.canvasController.isActualViewBox(viewBox);
                 }
-                return !this.canvasController.isActualViewBox(viewBox);
               },
               shortCut: { keysOrCodes: "0", metaKey: true, globalOverride: true },
               callback: () => {
