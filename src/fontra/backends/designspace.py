88f12e02410f6d6166eab3e994f103e5b567f5a7
Olli Meier <hallo@olli-meier.de>
2024-03-01 15:09:14 +0100

    Fixes a fail within the unittests.

--- src/fontra/backends/designspace.py
+++ src/fontra/backends/designspace.py
@@ -980,7 +980,7 @@ def readGlyphOrCreate(
         # We read the existing glyph so we don't lose any data that
         # Fontra doesn't understand
         glyphSet.readGlyph(glyphName, layerGlyph, validate=False)
-    layerGlyph.codePoints = codePoints
+    layerGlyph.unicodes = codePoints
     return layerGlyph
 
 
