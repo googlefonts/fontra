c7611c94b392169bc0201b1bc97f1cf2e7db288c
Just van Rossum <justvanrossum@gmail.com>
2024-03-04 21:41:43 +0100

    Add workflow backend

---
+++ src/fontra/backends/workflow.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+import yaml
+
+from ..core.classes import GlobalAxis, GlobalDiscreteAxis, VariableGlyph
+from ..core.protocols import ReadableFontBackend
+from ..workflow.workflow import Workflow
+
+
+@dataclass(kw_only=True)
+class WorkflowBackend:
+    workflow: Workflow
+    context: Any = None
+    endPoint: ReadableFontBackend | None = field(init=False, default=None)
+
+    @classmethod
+    def fromPath(cls, path):
+        config = yaml.safe_load(path.read_text())
+        return cls(workflow=Workflow(config=config))
+
+    async def _ensureSetup(self) -> ReadableFontBackend:
+        if self.endPoint is None:
+            self.context = self.workflow.endPoints()
+            endPoints = await self.context.__aenter__()
+            self.endPoint = endPoints.endPoint
+            assert self.endPoint is not None
+        return self.endPoint
+
+    def close(self) -> None:
+        pass
+
+    async def aclose(self) -> None:
+        await self.context.__aexit__()
+
+    async def getGlyph(self, glyphName: str) -> VariableGlyph | None:
+        endPoint = await self._ensureSetup()
+        return await endPoint.getGlyph(glyphName)
+
+    async def getGlobalAxes(self) -> list[GlobalAxis | GlobalDiscreteAxis]:
+        endPoint = await self._ensureSetup()
+        return await endPoint.getGlobalAxes()
+
+    async def getGlyphMap(self) -> dict[str, list[int]]:
+        endPoint = await self._ensureSetup()
+        return await endPoint.getGlyphMap()
+
+    async def getCustomData(self) -> dict[str, Any]:
+        endPoint = await self._ensureSetup()
+        return await endPoint.getCustomData()
+
+    async def getUnitsPerEm(self) -> int:
+        endPoint = await self._ensureSetup()
+        return await endPoint.getUnitsPerEm()
