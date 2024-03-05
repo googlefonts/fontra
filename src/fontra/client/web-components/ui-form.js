71cd8ef1aeff306231abc3d3db3b1588d34f469f
Olli Meier <hallo@olli-meier.de>
2024-03-05 12:40:02 +0100

    Make number inputs not editable if empty.

--- src/fontra/client/web-components/ui-form.js
+++ src/fontra/client/web-components/ui-form.js
@@ -207,7 +207,11 @@ export class Form extends SimpleElement {
       this._fieldChanging(fieldItem, value, undefined);
     };
     this._fieldGetters[fieldItem.key] = () => inputElement.value;
-    console.log("inputElement.value", inputElement.value);
+    if (inputElement.value === "") {
+      inputElement.disabled = "disabled";
+    } else {
+      inputElement.disabled = "";
+    }
     this._fieldSetters[fieldItem.key] = (value) => (inputElement.value = value);
     valueElement.appendChild(inputElement);
   }
