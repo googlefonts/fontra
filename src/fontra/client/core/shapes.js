c9408b0cece584b060b356f4a9181a72645d716f
Olli Meier <hallo@olli-meier.de>
2024-03-19 17:18:43 +0100

    Remove unnecessary SVG and improve CSS

--- src/fontra/client/core/shapes.js
+++
@@ -1,3 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/ellipse
-// ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle)
-// ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, counterclockwise)
