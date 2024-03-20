e673f38ad0d34b35bd6108f58a8686d57cf62b48
Olli Meier <hallo@olli-meier.de>
2024-03-20 16:21:13 +0100

    Removing unused imports

--- src/fontra/views/editor/edit-tools-shape-ellipse.js
+++ src/fontra/views/editor/edit-tools-shape-ellipse.js
@@ -1,5 +1,3 @@
-import * as rectangle from "../core/rectangle.js";
-import { range } from "../core/utils.js";
 import { VarPackedPath } from "../core/var-path.js";
 import { ShapeToolRect } from "./edit-tools-shape.js";
 
