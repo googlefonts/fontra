916658418f09bf0cd2c164808c3d5011d75e80b5
Olli Meier <hallo@olli-meier.de>
2024-04-09 12:54:24 +0200

    undo participatingPointIndices

--- src/fontra/views/editor/edit-behavior.js
+++ src/fontra/views/editor/edit-behavior.js
@@ -88,7 +88,6 @@ class EditBehavior {
       behavior
     );
     this.pointEditFuncs = pointEditFuncs;
-    this.participatingPointIndices = participatingPointIndices;
 
     const componentRollbackChanges = [];
     this.componentEditFuncs = [];
