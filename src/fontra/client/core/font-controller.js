2f8c631b2142c6822147d7beda8e05c11bf0f78d
Just van Rossum <justvanrossum@gmail.com>
2024-04-14 07:58:08 +0200

    Adapt to new Axes data structure

--- src/fontra/client/core/font-controller.js
+++ src/fontra/client/core/font-controller.js
@@ -41,7 +41,7 @@ export class FontController {
     this.characterMap = makeCharacterMapFromGlyphMap(glyphMap, false);
     this._rootObject = {};
     this._rootObject["glyphMap"] = getGlyphMapProxy(glyphMap, this.characterMap);
-    this._rootObject["axes"] = await this.font.getGlobalAxes();
+    this._rootObject["axes"] = await this.font.getAxes();
     this._rootObject["unitsPerEm"] = await this.font.getUnitsPerEm();
     this._rootObject["customData"] = await this.font.getCustomData();
     this._rootClassDef = (await getClassSchema())["Font"];
@@ -72,7 +72,7 @@ export class FontController {
   }
 
   get globalAxes() {
-    return this._rootObject["axes"];
+    return this._rootObject.axes.axes;
   }
 
   get unitsPerEm() {
