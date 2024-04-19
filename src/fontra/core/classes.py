a8f4df1df23484ff1f745203d8b3455faefb3741
Olli Meier <hallo@olli-meier.de>
2024-04-19 14:52:43 +0200

    Removing None for optional anchor name makes it sort to top
    + update E json file

--- src/fontra/core/classes.py
+++ src/fontra/core/classes.py
@@ -218,7 +218,7 @@ class Component:
 
 @dataclass(kw_only=True)
 class Anchor:
-    name: Optional[str] = None
+    name: Optional[str]
     x: float
     y: float
     customData: CustomData = field(default_factory=dict)
