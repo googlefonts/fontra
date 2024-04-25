c7ad58530b1f4d02edee2400c0875f34f2865722
Olli Meier <hallo@olli-meier.de>
2024-04-25 15:14:42 +0200

    Remove unused import

--- src/fontra/views/editor/edit-behavior.js
+++ src/fontra/views/editor/edit-behavior.js
@@ -1,7 +1,7 @@
 import { consolidateChanges } from "../core/changes.js";
 import { polygonIsConvex } from "../core/convex-hull.js";
 import { Transform, decomposedToTransform } from "../core/transform.js";
-import { enumerate, parseSelection, reversed, unionIndexSets } from "../core/utils.js";
+import { parseSelection, reversed, unionIndexSets } from "../core/utils.js";
 import { copyComponent } from "../core/var-glyph.js";
 import * as vector from "../core/vector.js";
 import {
