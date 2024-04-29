f3963df3c80dd51b00fc8f831692cadcce62f736
Just van Rossum <justvanrossum@gmail.com>
2024-04-29 09:50:10 +0200

    Add helper module for manipulating OT features

---
+++ src/fontra/workflow/features.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Iterable
+
+import ufomerge
+
+from fontra.core.classes import OpenTypeFeatures
+
+
+def mergeFeatures(
+    featureTextA: str,
+    glyphMapA: dict[str, list[int]],
+    featureTextB: str,
+    glyphMapB: dict[str, list[int]],
+) -> tuple[str, dict[str, list[int]]]:
+    ufoA = MinimalUFO(glyphMap=glyphMapA, features=OpenTypeFeatures(text=featureTextA))
+    ufoB = MinimalUFO(glyphMap=glyphMapB, features=OpenTypeFeatures(text=featureTextB))
+
+    merger = ufomerge.UFOMerger(ufoA, ufoB)
+    merger.merge()
+
+    return ufoA.features.text, ufoA.getMergedGlyphMap()
+
+
+def subsetFeatures(
+    featureText: str,
+    glyphMap: dict[str, list[int]],
+    keepGlyphNames: Iterable[str],
+    layoutHandling="subset",
+) -> tuple[str, dict[str, list[int]]]:
+    ufo = MinimalUFO(glyphMap=glyphMap, features=OpenTypeFeatures(text=featureText))
+
+    subsettedUFO = _subset_ufo(
+        ufo, glyphs=keepGlyphNames, layout_handling=layoutHandling
+    )
+    return subsettedUFO.features.text, subsettedUFO.getMergedGlyphMap()
+
+
+@dataclass(kw_only=True)
+class MinimalGlyph:
+    name: str
+    unicodes: list[int] = field(default_factory=list)
+    components: list = field(default_factory=list)
+
+
+@dataclass(kw_only=True)
+class MinimalUFO:
+    glyphMap: dict[str, list[int]] = field(default_factory=dict)
+    features: OpenTypeFeatures = field(default_factory=OpenTypeFeatures)
+    layers: dict = field(init=False, repr=False, default_factory=dict)
+    groups: dict = field(init=False, repr=False, default_factory=dict)
+    kerning: dict = field(init=False, repr=False, default_factory=dict)
+    lib: dict = field(init=False, repr=False, default_factory=dict)
+    updatedGlyphMap: dict[str, list[int]] = field(
+        init=False, repr=False, default_factory=dict
+    )
+
+    def keys(self) -> Iterable[str]:
+        return self.glyphMap.keys()
+
+    def __contains__(self, glyphName: str) -> bool:
+        return glyphName in self.glyphMap
+
+    def __iter__(self) -> Iterable[MinimalGlyph]:
+        for glyphName, codePoints in self.glyphMap.items():
+            yield MinimalGlyph(name=glyphName, unicodes=codePoints)
+
+    def __getitem__(self, glyphName: str) -> MinimalGlyph:
+        if glyphName not in self.glyphMap:
+            raise KeyError(glyphName)
+        return MinimalGlyph(name=glyphName, unicodes=self.glyphMap[glyphName])
+
+    def __setitem__(self, glyphName: str, glyph: MinimalGlyph) -> None:
+        assert glyph.name == glyphName
+        self.addGlyph(glyph)
+
+    def addGlyph(self, glyph: MinimalGlyph) -> None:
+        self.updatedGlyphMap[glyph.name] = glyph.unicodes
+
+    def getMergedGlyphMap(self) -> dict[str, list[int]]:
+        return self.glyphMap | self.updatedGlyphMap
+
+
+def _subset_ufo(
+    ufo: MinimalUFO,
+    glyphs: Iterable[str] = [],
+    exclude_glyphs: Iterable[str] = [],
+    codepoints: Iterable[int] = [],
+    layout_handling: str = "subset",
+    include_dir: Path | None = None,
+    original_glyphlist: Iterable[str] | None = None,
+) -> MinimalUFO:
+    new_ufo = MinimalUFO()
+    ufomerge.merge_ufos(
+        new_ufo,
+        ufo,
+        glyphs,
+        exclude_glyphs,
+        codepoints,
+        layout_handling=layout_handling,
+        include_dir=include_dir,
+        original_glyphlist=original_glyphlist,
+    )
+    return new_ufo
