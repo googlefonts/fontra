c024d3f5234fd4395bc60edae33b9f5cb5dbca91
Olli Meier <hallo@olli-meier.de>
2024-05-02 17:37:03 +0200

    minor changes to unittests

--- test-py/test_backends_designspace.py
+++ test-py/test_backends_designspace.py
@@ -222,14 +222,14 @@ async def test_addAnchor(writableTestFont):
     )
 
 
-async def test_read_glyph_locked(writableTestFont):
+async def test_read_glyph_locked(testFont):
     glyphName = "space"
-    glyph = await writableTestFont.getGlyph(glyphName)
+    glyph = await testFont.getGlyph(glyphName)
 
     assert glyph.customData.get("fontra.glyph.locked") is True
 
 
-async def test_add_glyph_locked(writableTestFont):
+async def test_write_glyph_locked(writableTestFont):
     glyphName = "space"
     glyphMap = await writableTestFont.getGlyphMap()
     glyph = await writableTestFont.getGlyph(glyphName)
