8c9c6e6a634470cbfd5b6ec31f0f8338799efed8
Just van Rossum <justvanrossum@gmail.com>
2024-05-07 20:56:11 +0200

    Beginnings of accordion ui

---
+++ src/fontra/client/web-components/ui-accordion.js
@@ -0,0 +1,114 @@
+import * as html from "/core/html-utils.js";
+import { UnlitElement } from "/core/html-utils.js";
+import { enumerate } from "/core/utils.js";
+
+export class Accordion extends UnlitElement {
+  static styles = `
+  .ui-accordion-contents {
+    padding: 1em;
+    display: grid;
+    grid-template-rows: auto;
+    align-content: start;
+    align-items: start;
+    gap: 0.5em;
+    text-wrap: wrap;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  .ui-accordion-item {
+    display: grid;
+    grid-template-rows: auto 1fr;
+    gap: 0.2em;
+    min-height: 0;
+  }
+
+  .ui-accordion-item[hidden] {
+    display: none;
+  }
+
+  .ui-accordion-item-header {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    justify-content: start;
+    align-items: center;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .ui-accordion-item .open-close-icon {
+    height: 1.5em;
+    width: 1.5em;
+    transform: rotate(180deg);
+    transition: 120ms;
+  }
+
+  .ui-accordion-item.ui-accordion-item-closed .open-close-icon {
+    transform: rotate(0deg);
+  }
+
+  .ui-accordion-item.ui-accordion-item-closed .ui-accordion-item-content {
+    display: none;
+  }
+
+  .ui-accordion-item-content {
+    display: block;
+    // min-height: 0;
+    box-sizing: border-box;
+    overflow: auto;
+  }
+  `;
+
+  static properties = {
+    items: { type: Array },
+  };
+
+  render() {
+    const itemElements = [];
+    for (const [index, item] of enumerate(this.items || [])) {
+      const id = item.id || `ui-accordion-item-${index}`;
+
+      const headerElement = html.div(
+        {
+          class: "ui-accordion-item-header",
+          onclick: (event) => this._toggleItem(itemElement),
+        },
+        [
+          html.createDomElement("inline-svg", {
+            class: "open-close-icon",
+            src: "/tabler-icons/chevron-up.svg",
+          }),
+          item.label,
+        ]
+      );
+
+      const contentElement = html.div(
+        { class: "ui-accordion-item-content", hidden: !item.open },
+        [item.content]
+      );
+
+      const itemElement = html.div(
+        { class: "ui-accordion-item", id: id, hidden: !!item.hidden },
+        [headerElement, contentElement]
+      );
+
+      if (!item.open) {
+        itemElement.classList.add("ui-accordion-item-closed");
+      }
+
+      itemElements.push(itemElement);
+    }
+    return html.div({ class: "ui-accordion-contents" }, itemElements);
+  }
+
+  getItemElement(itemID) {
+    return this.shadowRoot.querySelector(`#${itemID}`);
+  }
+
+  _toggleItem(itemElement) {
+    itemElement.classList.toggle("ui-accordion-item-closed");
+  }
+}
+
+customElements.define("ui-accordion", Accordion);
