a28853e2dfca452620009ef9c91148dc12b20c17
Aaron Ruan <aaron212cn@outlook.com>
2024-05-21 23:01:11 +0800

    add support for localizing string templates and plural forms

--- src/fontra/client/core/localization.js
+++ src/fontra/client/core/localization.js
@@ -15,17 +15,65 @@ function languageChanged(locale) {
 
 languageController.addKeyListener("language", (event) => {
   languageChanged(languageController.model.language);
+  // TODO: Implement a better way to localize the page
+  // This method does not work perfectly on Chrome (and Edge maybe)
   window.location.reload(true);
 });
 
 languageChanged(languageController.model.language || "en");
 
-export function translate(key) {
-  if (debugTranslation) {
+function formatString(template, ...args) {
+  return template.replace(/%\d+/g, (match) => {
+    const index = parseInt(match.substring(1), 10);
+    console.log(args.length);
+
+    // Validation: Check that the index is a non-negative integer, and within bounds
+    return Number.isInteger(index) && index >= 0 && index < args.length
+      ? String(args[index])
+      : match;
+  });
+}
+
+export function translate(key, ...args) {
+  /**
+   * Translate the key to the corresponding value in the localizationData
+   * @param {string} key - The key to translate
+   * @param {...string} [args] - The arguments to replace in the translated value
+   * @returns {string} The translated value
+   */
+
+  const translation = localizationData[key];
+
+  if (typeof key !== "string" || translation === undefined || debugTranslation) {
     return key;
   }
 
-  return localizationData[key] || `!${key}!`;
+  return args.length > 0 ? formatString(translation, ...args) : translation;
+}
+
+export function translatePlural(key, quantity = 0) {
+  /**
+   * Translate the key to the corresponding value in the localizationData, with pluralization
+   * @param {string} key - The key to translate
+   * @param {number} quantity - The argument to replace in the translated value
+   * @returns {string} The translated value
+   */
+
+  const translation = localizationData[key];
+
+  if (typeof key !== "string" || translation === undefined || debugTranslation) {
+    return key;
+  }
+
+  const translationPlural = localizationData[key + ".plural"];
+
+  if (translationPlural === undefined) {
+    return formatString(translation, quantity);
+  } else {
+    return Math.abs(quantity) == 1
+      ? formatString(translation, quantity)
+      : formatString(translationPlural, quantity);
+  }
 }
 
 export function localizePage() {
