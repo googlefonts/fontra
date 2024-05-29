2cdd94dc568801e8e122baec143941b00e0406a3
Just van Rossum <justvanrossum@gmail.com>
2024-05-29 11:38:47 +0200

    Ensure that only font axis values can be inherited down the component chain

--- src/fontra/client/core/glyph-controller.js
+++ src/fontra/client/core/glyph-controller.js
@@ -36,6 +36,7 @@ export class VariableGlyphController {
   constructor(glyph, fontAxes) {
     this.glyph = glyph;
     this._fontAxesSourceSpace = mapAxesFromUserSpaceToSourceSpace(fontAxes);
+    this._fontAxisNames = new Set(fontAxes.map((axis) => axis.name));
     this._locationToSourceIndex = {};
     this._layerGlyphControllers = {};
   }
@@ -352,7 +353,8 @@ export class VariableGlyphController {
         );
         await instanceController.setupComponents(
           getGlyphFunc,
-          this.sources[sourceIndex].location
+          filterLocation(this.sources[sourceIndex].location, this._fontAxisNames),
+          this._fontAxisNames
         );
       } else {
         instanceController = null;
@@ -410,7 +412,11 @@ export class VariableGlyphController {
       errors
     );
 
-    await instanceController.setupComponents(getGlyphFunc, sourceLocation);
+    await instanceController.setupComponents(
+      getGlyphFunc,
+      filterLocation(sourceLocation, this._fontAxisNames),
+      this._fontAxisNames
+    );
     return instanceController;
   }
 
@@ -467,14 +473,17 @@ export class StaticGlyphController {
     this.components = [];
   }
 
-  async setupComponents(getGlyphFunc, parentLocation) {
+  async setupComponents(getGlyphFunc, parentLocation, fontAxisNames) {
     this.components = [];
     const componentErrors = [];
     for (const compo of this.instance.components) {
       const compoController = new ComponentController(compo);
-      const errors = await compoController.setupPath(getGlyphFunc, parentLocation, [
-        this.name,
-      ]);
+      const errors = await compoController.setupPath(
+        getGlyphFunc,
+        parentLocation,
+        [this.name],
+        fontAxisNames
+      );
       if (errors) {
         componentErrors.push(...errors);
       }
@@ -684,12 +693,13 @@ class ComponentController {
     this.compo = compo;
   }
 
-  async setupPath(getGlyphFunc, parentLocation, parentGlyphNames) {
+  async setupPath(getGlyphFunc, parentLocation, parentGlyphNames, fontAxisNames) {
     const { path, errors } = await flattenComponent(
       this.compo,
       getGlyphFunc,
       parentLocation,
-      parentGlyphNames
+      parentGlyphNames,
+      fontAxisNames
     );
     this.path = path;
     return errors;
@@ -753,14 +763,21 @@ class ComponentController {
   }
 }
 
-async function flattenComponent(compo, getGlyphFunc, parentLocation, parentGlyphNames) {
+async function flattenComponent(
+  compo,
+  getGlyphFunc,
+  parentLocation,
+  parentGlyphNames,
+  fontAxisNames
+) {
   let componentErrors = [];
   const paths = [];
   for await (const { path, errors } of iterFlattenedComponentPaths(
     compo,
     getGlyphFunc,
     parentLocation,
-    parentGlyphNames
+    parentGlyphNames,
+    fontAxisNames
   )) {
     paths.push(path);
     if (errors) {
@@ -778,6 +795,7 @@ async function* iterFlattenedComponentPaths(
   getGlyphFunc,
   parentLocation,
   parentGlyphNames,
+  fontAxisNames,
   transformation = null,
   seenGlyphNames = null
 ) {
@@ -818,8 +836,9 @@ async function* iterFlattenedComponentPaths(
     yield* iterFlattenedComponentPaths(
       subCompo,
       getGlyphFunc,
-      compoLocation,
+      filterLocation(compoLocation, fontAxisNames),
       parentGlyphNames,
+      fontAxisNames,
       t,
       seenGlyphNames
     );
@@ -916,6 +935,12 @@ function mergeLocations(loc1, loc2) {
     return loc2 || {};
   }
   return { ...loc1, ...loc2 };
+}
+
+function filterLocation(loc, axisNames) {
+  return Object.fromEntries(
+    Object.entries(loc).filter((entry) => axisNames.has(entry[0]))
+  );
 }
 
 function subsetLocation(location, axes) {
