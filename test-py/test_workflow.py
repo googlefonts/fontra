aa3f7c4676a67626dde2ac02b98ee6d561da421c
Just van Rossum <justvanrossum@gmail.com>
2024-06-19 23:18:13 +0200

    Add fixWithFallback option to check-interpolation

--- test-py/test_workflow.py
+++ test-py/test_workflow.py
@@ -737,6 +737,28 @@ def test_command(tmpdir, configYAMLSources):
             ],
         ),
         (
+            "check-interpolation-no-fail",
+            """
+            steps:
+            - input: fontra-read
+              source: "test-py/data/workflow/input-check-interpolation.fontra"
+
+            - filter: check-interpolation
+              fixWithFallback: true
+
+            - output: fontra-write
+              destination: "output-check-interpolation-no-fail.fontra"
+            """,
+            True,
+            [
+                (
+                    40,
+                    "check-interpolation: glyph A can't be interpolated "
+                    "InterpolationError('paths are not compatible')",
+                ),
+            ],
+        ),
+        (
             "merge-codepoint-conflict",
             """
             steps:
