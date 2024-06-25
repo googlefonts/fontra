551880ece894750ade2819ae09b2efb5c112d931
Olli Meier <hallo@olli-meier.de>
2024-06-25 16:02:50 +0200

    First draft of glyph notes panel

--- src/fontra/views/editor/editor.js
+++ src/fontra/views/editor/editor.js
@@ -70,6 +70,7 @@ import { dialog, dialogSetup, message } from "/web-components/modal-dialog.js";
 import { parsePluginBasePath } from "/web-components/plugin-manager.js";
 
 import DesignspaceNavigationPanel from "./panel-designspace-navigation.js";
+import GlyphNotesPanel from "./panel-glyph-notes.js";
 import GlyphSearchPanel from "./panel-glyph-search.js";
 import ReferenceFontPanel from "./panel-reference-font.js";
 import SelectionInfoPanel from "./panel-selection-info.js";
@@ -819,6 +820,7 @@ export class EditorController {
     this.addSidebarPanel(new ReferenceFontPanel(this), "left");
     this.addSidebarPanel(new SelectionInfoPanel(this), "right");
     this.addSidebarPanel(new TransformationPanel(this), "right");
+    this.addSidebarPanel(new GlyphNotesPanel(this), "right");
 
     // Upon reload, the "animating" class may still be set (why?), so remove it
     for (const sidebarContainer of document.querySelectorAll(".sidebar-container")) {
