252a1d8c8e1b7a1176fec8c6dfa4713bfadd1480
Just van Rossum <justvanrossum@gmail.com>
2024-06-25 13:34:24 +0200

    Beginnings of unit tests for DiscreteVariationModel

---
+++ test-js/test-discrete-variation-model.js
@@ -0,0 +1,58 @@
+import { expect } from "chai";
+import { DiscreteVariationModel } from "../src/fontra/client/core/discrete-variation-model.js";
+import { parametrize } from "./test-support.js";
+
+describe("DiscreteVariationModel tests", () => {
+  const testAxes = [
+    { name: "Weight", minValue: 400, maxValue: 700, defaultValue: 400 },
+    { name: "Italic", values: [0, 1], defaultValue: 0 },
+  ];
+
+  const testLocations = [
+    {},
+    { Weight: 700 },
+    { Italic: 1 },
+    { Weight: 700, Italic: 1 },
+  ];
+
+  const testSourceData = [
+    [0, 0],
+    [100, 0],
+    [0, 100, 200], // Incompatible Italic sources
+    [100, 100, 300], // etc.
+  ];
+
+  const testCases = [
+    {
+      location: {},
+      expectedResult: [0, 0],
+    },
+    {
+      location: { Weight: 550 },
+      expectedResult: [50, 0],
+    },
+    {
+      location: { Weight: 700 },
+      expectedResult: [100, 0],
+    },
+    {
+      location: { Italic: 1 },
+      expectedResult: [0, 100, 200],
+    },
+    {
+      location: { Weight: 700, Italic: 1 },
+      expectedResult: [100, 100, 300],
+    },
+    {
+      location: { Weight: 550, Italic: 1 },
+      expectedResult: [50, 100, 250],
+    },
+  ];
+
+  parametrize("DiscreteVariationModel test", testCases, (testData) => {
+    const model = new DiscreteVariationModel(testLocations, testAxes);
+    const deltas = model.getDeltas(testSourceData);
+    const { instance, errors } = model.interpolateFromDeltas(testData.location, deltas);
+    expect(instance).to.deep.equal(testData.expectedResult);
+  });
+});
