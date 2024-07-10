df942eb92084ad472105b861fb8c3c3d3b63c6ea
Olli Meier <hallo@olli-meier.de>
2024-07-10 13:41:01 +0200

    KnifeTool: use pathHitTester + visualize intersection points

--- src/fontra/views/editor/edit-tools-knife.js
+++ src/fontra/views/editor/edit-tools-knife.js
@@ -3,6 +3,7 @@ import { range } from "../core/utils.js";
 import { VarPackedPath, packContour } from "../core/var-path.js";
 import { BaseTool, shouldInitiateDrag } from "./edit-tools-base.js";
 import {
+  fillRoundNode,
   registerVisualizationLayerDefinition,
   strokeLine,
 } from "./visualization-layer-definitions.js";
@@ -31,16 +32,18 @@ export class KnifeTool extends BaseTool {
       return;
     }
 
-    const pointA = this.sceneController.selectedGlyphPoint(initialEvent);
-    this.sceneModel.knifeToolPointA = pointA;
-
     if (!(await shouldInitiateDrag(eventStream, initialEvent))) {
       // TODO: open dialog for numeric size input
       return;
     }
 
+    const pointA = this.sceneController.selectedGlyphPoint(initialEvent);
+    this.sceneModel.knifeToolPointA = pointA;
+    const glyphController = await this.sceneModel.getSelectedStaticGlyphController();
+
     let eventTemp;
     let pointB;
+    let intersections;
     for await (const event of eventStream) {
       eventTemp = event;
       const point = this.sceneController.selectedGlyphPoint(event);
@@ -50,17 +53,21 @@ export class KnifeTool extends BaseTool {
       }
 
       this.sceneModel.knifeToolPointB = pointB = point;
+      this.sceneModel.intersections = intersections = getIntersections(
+        glyphController,
+        pointA,
+        pointB
+      );
       this.sceneModel.event = event;
       this.canvasController.requestUpdate();
     }
 
+    delete this.sceneModel.intersections;
     delete this.sceneModel.knifeToolPointB;
     delete this.sceneModel.event;
     this.canvasController.requestUpdate();
 
-    console.log(
-      `KnifeTool: cut from ${pointA.x}, ${pointA.y} to ${pointB.x}, ${pointB.y}`
-    );
+    console.log("KnifeTool intersections:", intersections);
     // TODO: cut the path with the knife tool
   }
 
@@ -74,9 +81,9 @@ registerVisualizationLayerDefinition({
   name: "Knife tool line",
   selectionMode: "editing",
   zIndex: 500,
-  screenParameters: { strokeWidth: 1 },
-  colors: { strokeColor: "#1118", color: "#000" },
-  colorsDarkMode: { strokeColor: "#FFFB", color: "#FFF" },
+  screenParameters: { strokeWidth: 1, nodeSize: 5 },
+  colors: { strokeColor: "#1118", nodeColor: "#000", color: "#000" },
+  colorsDarkMode: { strokeColor: "#FFFB", nodeColor: "#FFF", color: "#FFF" },
   draw: (context, positionedGlyph, parameters, model, controller) => {
     const pointA = model.knifeToolPointA;
     const pointB = model.knifeToolPointB;
@@ -87,5 +94,17 @@ registerVisualizationLayerDefinition({
     context.strokeStyle = parameters.strokeColor;
     context.lineWidth = parameters.strokeWidth;
     strokeLine(context, pointA.x, pointA.y, pointB.x, pointB.y);
+
+    context.fillStyle = parameters.nodeColor;
+    for (const intersection of model.intersections) {
+      fillRoundNode(context, intersection, parameters.nodeSize);
+    }
   },
 });
+
+function getIntersections(glyphController, p1, p2) {
+  // NOTE: Do we want to cut components as well? If so, we would need:
+  //const pathHitTester = glyphController.flattenedPathHitTester;
+  const pathHitTester = glyphController.pathHitTester;
+  return pathHitTester.lineIntersections(p1, p2, undefined, []);
+}
