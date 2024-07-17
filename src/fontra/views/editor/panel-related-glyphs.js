588c54a51dcecdf6ae65e5d7184f884c2fa2f156
Just van Rossum <justvanrossum@gmail.com>
2024-07-17 10:22:12 +0200

    Populate with some content

--- src/fontra/views/editor/panel-related-glyphs.js
+++ src/fontra/views/editor/panel-related-glyphs.js
@@ -1,6 +1,7 @@
 import Panel from "./panel.js";
 import * as html from "/core/html-utils.js";
-import { throttleCalls } from "/core/utils.js";
+import { getCharFromCodePoint, throttleCalls } from "/core/utils.js";
+import { Accordion } from "/web-components/ui-accordion.js";
 
 export default class RelatedGlyphPanel extends Panel {
   identifier = "related-glyphs";
@@ -11,10 +12,10 @@ export default class RelatedGlyphPanel extends Panel {
       box-sizing: border-box;
       height: 100%;
       width: 100%;
-      display: flex;
-      flex-direction: column;
-      gap: 0.5em;
-      padding: 1em;
+    }
+
+    #related-glyphs-header {
+      padding: 1em 1em 0 1em;
     }
   `;
 
@@ -24,46 +25,70 @@ export default class RelatedGlyphPanel extends Panel {
     this.fontController = this.editorController.fontController;
     this.sceneController = this.editorController.sceneController;
 
-    // this.setupGlyphRelationshipsElement();
+    this.setupGlyphRelationshipsElement();
 
-    // this.sceneController.sceneSettingsController.addKeyListener(
-    //   ["selectedGlyphName", "selection"],
-    //   (event) => this.throttledUpdate()
-    // );
-
-    // this.sceneController.addCurrentGlyphChangeListener((event) => {
-    //   this.throttledUpdate(event.senderID);
-    // });
+    this.sceneController.sceneSettingsController.addKeyListener(
+      ["selectedGlyphName"],
+      (event) => this.throttledUpdate()
+    );
   }
 
   getContentElement() {
+    const accordion = new Accordion();
+
+    this.glyphNameExtensionElement = html.div(
+      { id: "glyph-name-extension", style: "height: 100%;" },
+      []
+    );
+
+    accordion.items = [
+      {
+        id: "glyph-name-extension-accordion-item",
+        label: "Related glyphs via glyph name extension",
+        open: true,
+        content: this.glyphNameExtensionElement,
+      },
+    ];
     return html.div(
       {
         class: "sidebar-glyph-relationships",
       },
-      [
-        "Hewllo",
-        // html.div({ class: "glyph-note-header", id: "glyph-note-header" }, [
-        //   "Glyph note",
-        // ]),
-        // html.createDomElement("textarea", {
-        //   rows: 1,
-        //   wrap: "off",
-        //   id: "glyph-note-textarea",
-        // }),
-      ]
+      [html.div({ id: "related-glyphs-header" }, ["Related Glyphs"]), accordion]
     );
   }
 
   setupGlyphRelationshipsElement() {
-    //
+    this.relatedGlyphsHeaderElement = this.contentElement.querySelector(
+      "#related-glyphs-header"
+    );
   }
 
   async update() {
-    // const varGlyphController =
-    //   await this.sceneController.sceneModel.getSelectedVariableGlyphController();
-    // const varGlyph = varGlyphController?.glyph;
-    // this._selectedGlyphName = varGlyph?.name;
+    const varGlyphController =
+      await this.sceneController.sceneModel.getSelectedVariableGlyphController();
+    const varGlyph = varGlyphController?.glyph;
+
+    const glyphName = varGlyph?.name;
+    const codePoints = glyphName ? this.fontController.glyphMap[glyphName] : [];
+    const character = getCharFromCodePoint(codePoints[0]);
+    const s =
+      character && character != glyphName ? `“${character}”, ${glyphName}` : glyphName;
+
+    this.relatedGlyphsHeaderElement.innerHTML = glyphName
+      ? `<b>Related glyphs for ${s}</b>`
+      : `<b>Related glyphs</b> (no glyph selected)`;
+
+    if (glyphName) {
+      const relatedGlyphsByExtension = getRelatedGlyphsByExtension(
+        this.fontController.glyphMap,
+        glyphName
+      );
+      this.glyphNameExtensionElement.innerText = relatedGlyphsByExtension.length
+        ? relatedGlyphsByExtension.join(", ")
+        : "No related glyphs were found";
+    } else {
+      this.glyphNameExtensionElement.innerHTML;
+    }
   }
 
   async toggle(on, focus) {
@@ -71,6 +96,16 @@ export default class RelatedGlyphPanel extends Panel {
       this.update();
     }
   }
+}
+
+function getRelatedGlyphsByExtension(glyphMap, targetGlyphName) {
+  const targetBaseGlyphName = targetGlyphName.split(".")[0];
+  return Object.keys(glyphMap)
+    .filter((glyphName) => {
+      const baseGlyphName = glyphName.split(".")[0];
+      return baseGlyphName == targetBaseGlyphName && glyphName != targetGlyphName;
+    })
+    .sort();
 }
 
 customElements.define("panel-related-glyph", RelatedGlyphPanel);
