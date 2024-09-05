fdc5b22c818b9bda7d1190c3a355b98313b3d558
Olli Meier <hallo@olli-meier.de>
2024-09-05 12:16:12 +0200

    small refactoring of isMac

--- src/fontra/client/core/utils.js
+++ src/fontra/client/core/utils.js
@@ -120,13 +120,13 @@ export function hyphenatedToLabel(s) {
   return capitalizeFirstLetter(s).replaceAll("-", " ");
 }
 
-// navigator.platform is deprecated, please see:
+// platform is deprecated, please see:
 // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/platform
 // export const isMac = typeof navigator !== "undefined" && navigator.platform.toLowerCase().indexOf("mac") >= 0
 
 // Therefore use window.navigator https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
 export const isMac =
-  typeof window !== "undefined" && window.navigator.userAgent.indexOf("Mac") != -1;
+  typeof navigator !== "undefined" && navigator.userAgent.indexOf("Mac") != -1;
 
 // For several functions, we use the command key ("metaKey") on macOS,
 // and the control key ("ctrlKey") on non-macOS. For example short cuts
