77b3211feb628e7036a4d3f7b3c5755ae0e77264
Olli Meier <21055547+ollimeier@users.noreply.github.com>
2024-09-05 09:19:55 +0200

    Update src/fontra/views/applicationsettings/panel-shortcuts.js
    Co-authored-by: Just van Rossum <justvanrossum@gmail.com>

--- src/fontra/views/applicationsettings/panel-shortcuts.js
+++ src/fontra/views/applicationsettings/panel-shortcuts.js
@@ -137,7 +137,7 @@ export class ShortCutsPanel extends BaseInfoPanel {
       }
       shortCutsDataCustom[actionIdentifier] = shortCuts;
     }
-    const data = JSON.stringify(shortCutsDataCustom);
+    const data = JSON.stringify(shortCutsDataCustom, null, 2);
     const blob = new Blob([data], { type: "application/json" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
