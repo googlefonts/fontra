73792625b2f67ad4fbcfae74610ddca081a3f84c
Olli Meier <hallo@olli-meier.de>
2024-09-17 17:25:19 +0200

    Rename key to application-settings.server-info.title + use Chinese translation

--- src/fontra/views/applicationsettings/panel-server-info.js
+++ src/fontra/views/applicationsettings/panel-server-info.js
@@ -1,7 +1,7 @@
 import { BaseInfoPanel } from "./panel-base.js";
 
 export class ServerInfoPanel extends BaseInfoPanel {
-  static title = "server-info.title";
+  static title = "application-settings.server-info.title";
   static id = "server-info-panel";
 
   async setupUI() {
