b4b17a6b9541e3459e1dc052414e24cf0410508e
Olli Meier <hallo@olli-meier.de>
2024-10-10 12:38:29 +0200

    Use filteredGlyphItems for Next/Previous Glyph

--- src/fontra/client/web-components/glyphs-search.js
+++ src/fontra/client/web-components/glyphs-search.js
@@ -155,6 +155,7 @@ export class GlyphsSearch extends UnlitElement {
     const filteredGlyphItems = this.glyphsListItems.filter(
       this._glyphNamesListFilterFunc
     );
+    this.filteredGlyphItems = filteredGlyphItems;
     this.glyphNamesList.setItems(filteredGlyphItems);
   }
 }
