f3ac4c4e28fddff37a58648e646987ad8d9fcc26
Olli Meier <hallo@olli-meier.de>
2024-10-22 14:29:56 +0200

    get formatted location string for one-liner

--- src/fontra/views/fontinfo/panel-sources.js
+++ src/fontra/views/fontinfo/panel-sources.js
@@ -473,12 +473,12 @@ class SourceBox extends HTMLElement {
     }
   }
 
-  getNiceLocationString() {
+  getFormattedLocationString() {
     return (
-      " " +
-      locationToString(
-        makeSparseLocation(this.source.location, this.fontAxesSourceSpace)
-      )
+      " " + // add space to separate from the name
+      Object.entries(this.source.location)
+        .map(([axisName, value]) => `${axisName}=${value}`)
+        .join(", ")
     );
   }
 
@@ -539,7 +539,7 @@ class SourceBox extends HTMLElement {
     // This is the one-liner for the folded card –> only visible when folded.
     const tempElement = html.div({ class: "fontra-ui-font-info-sources-panel-temp" });
     tempElement.appendChild(html.span({ class: "bold" }, [this.source.name]));
-    tempElement.appendChild(html.span({}, [this.getNiceLocationString()]));
+    tempElement.appendChild(html.span({}, [this.getFormattedLocationString()]));
     this.append(tempElement);
 
     for (const key in models) {
