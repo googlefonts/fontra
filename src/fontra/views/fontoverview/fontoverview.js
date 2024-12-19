ee023d16ab2aec2a6bc9681153990c1b8b86a6b9
Just van Rossum <justvanrossum@gmail.com>
2024-12-19 16:02:43 +0100

    Use GlyphsSearchField

--- src/fontra/views/fontoverview/fontoverview.js
+++ src/fontra/views/fontoverview/fontoverview.js
@@ -13,6 +13,7 @@ import { mapAxesFromUserSpaceToSourceSpace } from "../core/var-model.js";
 import { makeDisplayPath } from "../core/view-utils.js";
 import { translate } from "/core/localization.js";
 import { GlyphCell } from "/web-components/glyph-cell.js";
+import { GlyphsSearchField } from "/web-components/glyphs-search-field.js";
 import { message } from "/web-components/modal-dialog.js";
 import { Accordion } from "/web-components/ui-accordion.js";
 
@@ -38,112 +39,6 @@ const colors = {
   "search-input-foreground-color": ["black", "white"],
   "search-input-background-color": ["#eee", "#333"],
 };
-
-class GlyphsSearchForOverview extends UnlitElement {
-  static styles = `
-    ${themeColorCSS(colors)}
-
-    :host {
-      display: grid;
-      grid-template-rows: auto 1fr;
-      box-sizing: border-box;
-      overflow: hidden;
-      align-content: start;
-    }
-
-    input {
-      color: var(--search-input-foreground-color);
-      background-color: var(--search-input-background-color);
-      font-family: fontra-ui-regular, sans-serif;
-      font-size: 1.1rem;
-      border-radius: 2em;
-      border: none;
-      outline: none;
-      resize: none;
-      width: 100%;
-      height: 1.8em;
-      box-sizing: border-box;
-      padding: 0.2em 0.8em;
-    }
-  `;
-
-  constructor(glyphsListItemsController, controllerKey) {
-    super();
-    this.glyphsListItemsController = glyphsListItemsController;
-    this.controllerKey = controllerKey;
-    this.searchField = html.input({
-      type: "text",
-      placeholder: translate("sidebar.glyphs.search"),
-      autocomplete: "off",
-      oninput: (event) => this._searchFieldChanged(event),
-    });
-
-    // I delete a big chunk of code here that is not needed for the overview
-
-    this._glyphNamesListFilterFunc = (item) => true; // pass all through
-
-    this.glyphMap = {};
-  }
-
-  focusSearchField() {
-    this.searchField.focus();
-  }
-
-  render() {
-    return this.searchField;
-  }
-
-  get glyphMap() {
-    return this._glyphMap;
-  }
-
-  set glyphMap(glyphMap) {
-    this._glyphMap = glyphMap;
-    this.updateGlyphNamesListContent();
-  }
-
-  // getSelectedGlyphName() {
-  //   return this.glyphNamesList.items[this.glyphNamesList.selectedItemIndex]?.glyphName;
-  // }
-
-  // getFilteredGlyphNames() {
-  //   return this.glyphNamesList.items.map((item) => item.glyphName);
-  // }
-
-  updateGlyphNamesListContent() {
-    const glyphMap = this.glyphMap;
-    this.glyphsListItems = [];
-    for (const glyphName in glyphMap) {
-      this.glyphsListItems.push({
-        glyphName: glyphName,
-        unicodes: glyphMap[glyphName],
-      });
-    }
-    this.glyphsListItems.sort(glyphItemSortFunc);
-    this._setFilteredGlyphNamesListContent();
-  }
-
-  _searchFieldChanged(event) {
-    const value = event.target.value;
-    const searchItems = value.split(/\s+/).filter((item) => item.length);
-    const hexSearchItems = searchItems
-      .filter((item) => [...item].length === 1) // num chars, not utf16 units!
-      .map((item) => item.codePointAt(0).toString(16).toUpperCase().padStart(4, "0"));
-    searchItems.push(...hexSearchItems);
-    this._glyphNamesListFilterFunc = (item) => glyphFilterFunc(item, searchItems);
-    this._setFilteredGlyphNamesListContent();
-  }
-
-  async _setFilteredGlyphNamesListContent() {
-    const filteredGlyphItems = this.glyphsListItems.filter(
-      this._glyphNamesListFilterFunc
-    );
-    //this.glyphNamesList.setItems(filteredGlyphItems);
-    this.glyphsListItemsController.model[this.controllerKey] = filteredGlyphItems;
-  }
-}
-
-customElements.define("glyphs-search-glyph-overview", GlyphsSearchForOverview);
 
 function glyphItemSortFunc(item1, item2) {
   const uniCmp = compare(item1.unicodes[0], item2.unicodes[0]);
@@ -303,7 +198,7 @@ export class FontOverviewController {
     );
 
     // glyph search
-    this.glyphsSearch = new GlyphsSearchForOverview(
+    this.glyphsSearch = new GlyphsSearchField(
       this.glyphsListItemsController,
       "glyphsListItems"
     );
