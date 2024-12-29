731e04a271d8ae786dec84ba9b9dc3cc66ccc50f
Just van Rossum <justvanrossum@gmail.com>
2024-12-29 21:39:45 +0100

    Fix copy/paste error

--- test-py/test_glyph_data_csv.py
+++ test-py/test_glyph_data_csv.py
@@ -9,4 +9,4 @@ def test_glyph_data_csv_needs_update():
     try:
         subprocess.run(f"python {scriptPath} --check", check=True, shell=True)
     except subprocess.CalledProcessError:
-        assert 0, f"unicode-utils.js is stale, please run ./scripts/{scriptPath.name}"
+        assert 0, f"glyph-data.csv is stale, please run ./scripts/{scriptPath.name}"
