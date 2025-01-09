f41a05368c25b3a937f6409f702e557f68c0d53d
Olli Meier <hallo@olli-meier.de>
2025-01-09 18:20:03 +0100

    Prepare possible Window menu

--- src/fontra/client/core/fontra-menus.js
+++ src/fontra/client/core/fontra-menus.js
@@ -8,6 +8,7 @@ const mapMenuItemKeyToFunction = {
   Edit: getEditMenuItems,
   View: getViewMenuItems,
   Glyph: getGlyphMenuItems,
+  Window: getWindowMenuItems,
 };
 
 export function makeFontraMenuBar(menuItemKeys, viewController) {
@@ -163,22 +164,27 @@ function getGlyphMenuItems() {
   return [];
 }
 
-// // Disable for now, as the font overview isn't yet minimally feature-complete
-// {
-//   title: translate("menubar.window"),
-//   enabled: () => true,
-//   getItems: () => {
-//     return [
-//       {
-//         title: translate("font-overview.title"),
-//         enabled: () => true,
-//         callback: () => {
-//           const url = new URL(window.location);
-//           url.pathname = url.pathname.replace("/editor/", "/fontoverview/");
-//           url.hash = ""; // remove any hash
-//           window.open(url.toString());
-//         },
-//       },
-//     ];
-//   },
-// },
+function getWindowMenuItems() {
+  return [
+    {
+      title: translate("font-overview.title"),
+      enabled: () => true,
+      callback: () => {
+        const url = new URL(window.location);
+        url.pathname = `/fontoverview/-/${url.pathname.split("/").slice(-1)[0]}`;
+        url.hash = ""; // remove any hash
+        window.open(url.toString());
+      },
+    },
+    {
+      title: translate("editor.title"),
+      enabled: () => true,
+      callback: () => {
+        const url = new URL(window.location);
+        url.pathname = `/editor/-/${url.pathname.split("/").slice(-1)[0]}`;
+        url.hash = ""; // remove any hash
+        window.open(url.toString());
+      },
+    },
+  ];
+}
