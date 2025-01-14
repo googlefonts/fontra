be0fbf481827417a34aa5ce364602a342af4940b
Just van Rossum <justvanrossum@gmail.com>
2025-01-14 11:00:37 +0100

    Inital QnD glyph set parser, just enough for the GF sets

--- src/fontra/client/core/parse-glyph-set.js
+++ src/fontra/client/core/parse-glyph-set.js
@@ -1,3 +1,5 @@
+import { getCodePointFromGlyphName } from "./glyph-data.js";
+
 export const glyphSetDataFormats = [
   { value: "auto-detect", label: "auto-detect" },
   { value: "glyph-names", label: "Glyph names (whitespace-separated)" },
@@ -5,7 +7,27 @@ export const glyphSetDataFormats = [
   { value: "tsv", label: "TSV (tab-separated)" },
 ];
 
-export function parseGlyphSet(text, dataFormat) {
-  // TODO
-  return [];
+export function parseGlyphSet(sourceData, dataFormat) {
+  sourceData = sourceData.replaceAll("\r\n", "\n"); // normalize line endings
+
+  // TODO: TSV/CSV, etc.
+
+  const glyphSet = [];
+  for (let line of sourceData.split("\n")) {
+    const commentIndex = line.indexOf("#");
+    if (commentIndex >= 0) {
+      line = line.slice(0, commentIndex);
+    }
+    line = line.trim();
+    if (!line) {
+      continue;
+    }
+
+    for (const glyphName of line.split(/\s+/)) {
+      const codePoint = getCodePointFromGlyphName(glyphName);
+      glyphSet.push({ glyphName, codePoints: codePoint ? [codePoint] : [] });
+    }
+  }
+
+  return glyphSet;
 }
