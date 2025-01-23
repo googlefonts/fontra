54d0edef0150f580774186984aeb911a11b00fea
Simon Cozens <simon@simon-cozens.org>
2025-01-23 21:41:17 +0000

    Move JS code from HTML to start.js

---
+++ src/fontra/views/fontinfo/start.js
@@ -0,0 +1,10 @@
+import "/core/theme-settings.js";
+import "/web-components/modal-dialog.js";
+
+import { FontInfoController } from "/fontinfo/fontinfo.js";
+
+async function startApp() {
+  window.fontInfoController = await FontInfoController.fromBackend();
+}
+
+startApp();
