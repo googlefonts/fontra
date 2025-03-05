f1d7e65c879798b6e2826829e4e96cfb4c55c39a
Olli Meier <hallo@olli-meier.de>
2025-03-05 09:00:21 +0100

    Use Accordion for FontInfo for hiding CustomData: because empty or not needed.

--- src-js/views-fontinfo/src/panel-font-info.js
+++ src-js/views-fontinfo/src/panel-font-info.js
@@ -7,6 +7,7 @@ import { ObservableController } from "@fontra/core/observable-object.js";
 import { DefaultFormatter } from "@fontra/core/ui-utils.js";
 import { CustomDataList } from "@fontra/web-components/custom-data-list.js";
 import { message } from "@fontra/web-components/modal-dialog.js";
+import { Accordion } from "@fontra/web-components/ui-accordion.js";
 import { Form } from "@fontra/web-components/ui-form.js";
 import { BaseInfoPanel } from "./panel-base.js";
 
@@ -43,7 +44,7 @@ const customDataAttributesSupported = [
 ];
 
 addStyleSheet(`
-.font-info-form-container {
+.font-info-container {
   background-color: var(--ui-element-background-color);
   border-radius: 0.5em;
   padding: 1em;
@@ -67,10 +68,6 @@ export class FontInfoPanel extends BaseInfoPanel {
 
   async setupUI() {
     const info = await this.fontController.getFontInfo();
-
-    const containerFontInfo = html.div({
-      class: "font-info-form-container",
-    });
 
     this.infoForm = new Form();
     this.infoForm.className = "fontra-ui-font-info-form";
@@ -116,12 +113,6 @@ export class FontInfoPanel extends BaseInfoPanel {
     });
 
     this.infoForm.setFieldDescriptions(formContents);
-    containerFontInfo.append(
-      html.div({ class: "fontra-ui-font-info-header" }, [
-        translate("sources.labels.general"), // TODO: translation
-      ]),
-      this.infoForm
-    );
 
     const cutomDataController = new ObservableController({ ...info.customData });
 
@@ -161,15 +152,30 @@ export class FontInfoPanel extends BaseInfoPanel {
       fontObject: info,
       supportedAttributes: customDataAttributesSupported,
     });
-    containerFontInfo.append(
-      html.div({ class: "fontra-ui-font-info-header", style: "padding-top: 1em;" }, [
-        translate("Custom Data"), // TODO: translation
-      ]),
-      customDataList
-    );
+
+    const accordion = new Accordion();
+    const accordionItems = [
+      {
+        label: translate("sources.labels.general"),
+        id: "general",
+        content: this.infoForm,
+        open: true,
+      },
+      {
+        label: translate("Custom Data"), // TODO: translate
+        id: "custom-data",
+        content: customDataList,
+        open: info.customData || false,
+      },
+    ];
+
+    accordion.items = accordionItems;
 
     this.panelElement.innerHTML = "";
-    this.panelElement.appendChild(containerFontInfo);
+    this.panelElement.appendChild(
+      html.div({ class: "font-info-container" }, [accordion])
+    );
+    // this.panelElement.appendChild(containerFontInfo);
     this.panelElement.focus();
   }
 
