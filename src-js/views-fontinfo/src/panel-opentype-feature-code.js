f929e9e4cc3e4956563b86b210043aa5eec706bb
Herlan <herlannawwi515@gmail.com>
2025-07-06 02:27:00 +0700

    Add Codemirror legacy modes and Lezer highlight dependencies; enhance OpenType feature code panel with syntax highlighting, improve styling, and add indentWithTab support

--- src-js/views-fontinfo/src/panel-opentype-feature-code.js
+++ src-js/views-fontinfo/src/panel-opentype-feature-code.js
@@ -1,12 +1,43 @@
 import { autocompletion } from "@codemirror/autocomplete";
-import { history, redo, redoDepth, undo, undoDepth } from "@codemirror/history";
+import {
+  history,
+  indentWithTab,
+  redo,
+  redoDepth,
+  undo,
+  undoDepth,
+} from "@codemirror/commands";
+import {
+  HighlightStyle,
+  LanguageSupport,
+  StreamLanguage,
+  syntaxHighlighting,
+} from "@codemirror/language";
+import { simpleMode } from "@codemirror/legacy-modes/mode/simple-mode";
+import { EditorView, keymap } from "@codemirror/view";
 import * as html from "@fontra/core/html-utils.js";
 import { addStyleSheet } from "@fontra/core/html-utils.js";
 import { scheduleCalls } from "@fontra/core/utils.js";
-import { basicSetup, EditorView } from "codemirror";
+import { Tag } from "@lezer/highlight";
+import { basicSetup } from "codemirror";
 import { BaseInfoPanel } from "./panel-base.js";
 
 addStyleSheet(`
+:root {
+  --comment-color-light: var(--fontra-theme-marker) #676e78;
+  --comment-color-dark: #a2a2a2;
+  --keyword-color-light: var(--fontra-theme-marker) #0b57d0;
+  --keyword-color-dark: #75bfff;
+  --glyph-class-color-light: var(--fontra-theme-marker) #b90063;
+  --glyph-class-color-dark: #ff7de9;
+  --named-glyph-class-color-light: var(--fontra-theme-marker) #198639;
+  --named-glyph-class-color-dark: #86de74;
+
+  --comment-color: var(--comment-color-light, var(--comment-color-dark));
+  --keyword-color: var(--keyword-color-light, var(--keyword-color-dark));
+  --glyph-class-color: var(--glyph-class-color-light, var(--glyph-class-color-dark));
+  --named-glyph-class-color: var(--named-glyph-class-color-light, var(--named-glyph-class-color-dark));
+}
 
 #opentype-feature-code-panel.font-info-panel {
   height: 100%;
@@ -26,15 +57,77 @@ addStyleSheet(`
 
 #font-info-opentype-feature-code-text-entry-textarea {
   font-size: 1.1em;
-  overflow: scroll;
-  height: calc(100% - 2em);
+  height: calc(100% - 2.4em);
 }
 
 #font-info-opentype-feature-code-text-entry-textarea > .cm-editor {
   height: 100%;
 }
 
+#font-info-opentype-feature-code-text-entry-textarea > .cm-scroller {
+  overflow: auto;
+}
+
+#font-info-opentype-feature-code-text-entry-textarea > .cm-editor.cm-focused {
+  outline: none;
+}
+
 `);
+
+const openTypeFeatureCodeSimpleMode = simpleMode({
+  start: [
+    { regex: /#.*/, token: "comment" },
+    {
+      regex:
+        /\b(?:anchor|anchorDef|anon|anonymous|by|contourpoint|cursive|device|enum|enumerate|exclude_dflt|feature|from|ignore|IgnoreBaseGlyphs|IgnoreLigatures|IgnoreMarks|include|include_dflt|language|languagesystem|lookup|lookupflag|mark|MarkAttachmentType|markClass|nameid|NULL|parameters|pos|position|required|reversesub|RightToLeft|rsub|script|sub|substitute|subtable|table|useExtension|useMarkFilteringSet|valueRecordDef|excludeDFLT|includeDFLT)\b/,
+      token: "keyword",
+    },
+    {
+      regex: /\[\s*(?:[a-zA-Z0-9_.]+(?:\s*-\s*[a-zA-Z0-9_.]+)?\s*)*\]/,
+      token: "glyphClass",
+    },
+    { regex: /@[a-zA-Z0-9_.]+/, token: "namedGlyphClass" },
+  ],
+  languageData: {
+    commentTokens: { line: "#" },
+  },
+});
+
+openTypeFeatureCodeSimpleMode.tokenTable = {
+  comment: Tag.define(),
+  keyword: Tag.define(),
+  glyphClass: Tag.define(),
+  namedGlyphClass: Tag.define(),
+};
+
+const openTypeFeatureCodeStreamLanguage = StreamLanguage.define(
+  openTypeFeatureCodeSimpleMode
+);
+
+const openTypeFeatureCodeHighlighter = syntaxHighlighting(
+  HighlightStyle.define([
+    {
+      tag: openTypeFeatureCodeSimpleMode.tokenTable.comment,
+      color: "var(--comment-color)",
+    },
+    {
+      tag: openTypeFeatureCodeSimpleMode.tokenTable.keyword,
+      color: "var(--keyword-color)",
+    },
+    {
+      tag: openTypeFeatureCodeSimpleMode.tokenTable.glyphClass,
+      color: "var(--glyph-class-color)",
+    },
+    {
+      tag: openTypeFeatureCodeSimpleMode.tokenTable.namedGlyphClass,
+      color: "var(--named-glyph-class-color)",
+    },
+  ])
+);
+
+const openTypeFeatureLanguage = new LanguageSupport(openTypeFeatureCodeStreamLanguage, [
+  openTypeFeatureCodeHighlighter,
+]);
 
 const customTheme = EditorView.theme({
   ".cm-cursor": {
@@ -44,6 +137,34 @@ const customTheme = EditorView.theme({
     backgroundColor: "var(--ui-element-background-color)",
     color: "var(--horizontal-rule-color)",
     borderRight: "1px solid var(--horizontal-rule-color)",
+  },
+  ".cm-activeLine": {
+    backgroundColor: "#8D8D8D10",
+  },
+  ".cm-activeLineGutter": {
+    backgroundColor: "transparent",
+    color: "var(--ui-element-foreground-color)",
+  },
+  "&.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground": {
+    backgroundColor: "#D3E3FD",
+  },
+  ".cm-tooltip": {
+    backgroundColor: "var(--background-color)",
+    border: "0.5px solid gray",
+    borderRadius: "4px",
+    boxShadow: "2px 3px 10px #00000020",
+    padding: "0.5em 0",
+    fontFamily: "monospace",
+    fontSize: "1.1em",
+  },
+  ".cm-tooltip-autocomplete": {
+    "& > ul > li[aria-selected]": {
+      backgroundColor: "var(--fontra-red-color)",
+    },
+  },
+  ".cm-tooltip.cm-completionInfo": {
+    fontSize: "1em",
+    padding: "0.5em 0.8em 0.7em 0.8em",
   },
 });
 
@@ -76,6 +197,8 @@ export class OpenTypeFeatureCodePanel extends BaseInfoPanel {
     this.editorView = new EditorView({
       doc: features.text,
       extensions: [
+        openTypeFeatureLanguage,
+        keymap.of([indentWithTab]),
         basicSetup,
         customTheme,
         autocompletion({ override: [myCompletions] }),
@@ -132,10 +255,10 @@ const completions = [
     type: "keyword",
     info: `Example:
     feature case {
-        # lookups and rules
+      # lookups and rules
     } case;`,
     apply: `feature xxxx {
-    # lookups and rules
+  # lookups and rules
 } xxxx;`,
   },
   {
@@ -143,10 +266,10 @@ const completions = [
     type: "keyword",
     info: `Example:
     lookup LookupName {
-        # rules
+      # rules
     } LookupName;`,
     apply: `lookup LookupName {
-    # rules
+  # rules
 } LookupName`,
   },
   {
